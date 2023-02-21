e362cdd7162f214b51590745f0b78e6e139ad986
Pato <boatengpato.pb@gmail.com>
2023-02-21 23:42:50 +0000

    minor change

--- src/ucs_aashto_classification.py
+++ src/ucs_aashto_classification.py
@@ -125,26 +125,5 @@ class SoilData:
 @xw.arg("soil_parameters", np.array, ndim=1)
 def unified_classification(soil_parameters) -> str:
     soil = SoilData(*soil_parameters)
-    print(soil_parameters)
 
     return soil.get_unified_classification()
-
-
-if __name__ == "__main__":
-    soil_samples = [
-        (34.1, 21.1, 13, 47.88, 37.84, 14.28),
-        (27.5, 13.8, 13.7, 54.23, 45.69, 0.08),
-        (28.2, 14.6, 13.6, 56.43, 43.47, 0.1),
-        (28.5, 15.3, 13.2, 54.1, 45.71, 0.19),
-        (17.9, 15.9, 2, 20.99, 54.72, 24.29),
-        (15.1, 14.8, 0.27, 27.33, 61.8, 10.88),
-        (17.8, 15.5, 2.31, 45.35, 54.31, 0.35),
-        (53.5, 20.6, 33, 42.35, 57.3, 0.36),
-        (44.1, 27.3, 16.8, 43.21, 55.89, 0.89),
-        (37.9, 26.5, 11.3, 35.91, 63.77, 0.32),
-        (40.4, 23.3, 17, 37.97, 61.28, 0.75),
-    ]
-
-    print(
-        SoilData(*(15.1, 14.8, 0.27, 27.33, 61.8, 10.88)).get_unified_classification()
-    )
