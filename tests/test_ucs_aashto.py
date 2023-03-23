756f344809ed60ffa0d56ff342711e229f6acaf9
Pato <boatengpato.pb@gmail.com>
2023-03-23 20:59:50 +0000

    refactored the tests and renamed the test file

--- tests/test_ucs_aashto.py
+++
@@ -1,7 +0,0 @@
-from src.soil_classifier.soil_classifier import USCS
-from .conftest import soil_infos
-
-
-def test_USCS():
-    for soil_parameters, classification in soil_infos():
-        assert USCS(soil_parameters) == classification
---
+++ tests/test_uscs_aashto.py
@@ -0,0 +1,22 @@
+import pytest
+
+from src.soil_classifier.soil_classifier import USCS
+from .conftest import single_classification, dual_classification
+
+
+def _get_params(soil):
+    for soil_parameters in single_classification:
+        classification = soil_parameters.pop().strip()
+        soil_parameters = (float(soil_data) for soil_data in soil_parameters)
+    return soil_parameters, classification
+
+
+def test_single_classification(soil_infos):
+    soil_params, classification = _get_params(single_classification)
+    assert USCS(soil_parameters=soil_params) == classification
+
+
+@pytest.mark.xfail
+def test_dual_classification(soil_infos):
+    soil_params, classification = _get_params(dual_classification)
+    assert USCS(soil_parameters=soil_params) == classification
