77f6a26e5c992e2ef3d6c85651e55a69527691f0
Pato <boatengpato.pb@gmail.com>
2023-03-23 20:58:59 +0000

    added fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,17 +6,22 @@ import pytest
 TEST_DATA = pathlib.Path(__file__).parent / "test_data.csv"
 
 
+single_classification = []
+dual_classification = []
+
+
+@pytest.fixture(scope="session")
 def soil_infos():
-    with open(TEST_DATA, "r") as f:
-        soil_info = csv.reader(f)
-        next(soil_info)
+    f = open(TEST_DATA, "r")
+    soil_info = csv.reader(f)
+    next(soil_info)
 
-        for soil_data in soil_info:
-            classification = soil_data.pop()
-            if len(classification) > 2:
-                continue
+    for soil_data in soil_info:
+        if len(soil_data[-1].strip()) == 2:  # check classification type
+            single_classification.append(soil_data)
+        else:
+            dual_classification.append(soil_data)
 
-            soil_data = [
-                float(i) for i in soil_data
-            ]  # converting soil parameters to float
-            yield (soil_data, classification)
+    yield
+
+    f.close()
