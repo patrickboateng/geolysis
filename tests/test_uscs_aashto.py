b6227846c44ef6e042ee404f17ac7cea6b531ded
Pato <boatengpato.pb@gmail.com>
2023-03-31 10:30:29 +0000

    added tests for aashto classification

--- tests/test_uscs_aashto.py
+++ tests/test_uscs_aashto.py
@@ -9,21 +9,42 @@ class TestSoil:
     @pytest.fixture(scope="class")
     def soil(self) -> Soil:
         yield Soil(
+            liquid_limit=33,
+            plastic_limit=21,
+            plasticity_index=12,
+            fines=30,
+            sand=30,
+            gravel=40,
+        )
+
+    @pytest.fixture(scope="class")
+    def aashto_soil(self) -> Soil:
+        yield Soil(
             liquid_limit=70,
             plastic_limit=38,
             plasticity_index=32,
             fines=86,
-            sand=8,
-            gravel=6,
+            sand=7,
+            gravel=7,
         )
 
     def test_group_index(self, soil: Soil):
-        expected = 33.47
-        assert soil.group_index() == pytest.approx(expected=expected)
+        expected = 0.0
+        assert soil.group_index == pytest.approx(expected=expected)
 
     def test_aggregates(self):
         with pytest.raises(PSDValueError):
             Soil(33, 21, 12, 30, 30, 30)
+
+    def test_Aline(self, soil: Soil):
+        expected = 9.49
+        assert soil._A_line == pytest.approx(expected=expected)
+
+    def test_unified_classification(self, soil: Soil):
+        assert soil.get_unified_classification() == "GC"
+
+    def test_aashto_classification(self, aashto_soil: Soil):
+        assert aashto_soil.get_aashto_classification() == "A-7-5(33)"
 
 
 def _get_params(soil):
