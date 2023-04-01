953206c2b2c9443aa2e8c6e32514a9b67acb0c68
Pato <boatengpato.pb@gmail.com>
2023-04-01 18:57:44 +0000

    added PIValueError exception

--- src/geolab/exceptions.py
+++ src/geolab/exceptions.py
@@ -1,2 +1,9 @@
 class PSDValueError(ValueError):
-    """Fines, Sand, and Gravels should approximately sum up to 100%"""
+    """Fines, Sand, and Gravels should approximately sum up to 100%
+
+    `fines + sand + gravels = 100%`
+    """
+
+
+class PIValueError(ValueError):
+    """`Liquid Limit - Plastic Limit = Plasticity Index`"""
