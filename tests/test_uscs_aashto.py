86c820dcc2337180b23f6e4ddac92c20f012a3c3
Pato <boatengpato.pb@gmail.com>
2023-04-19 10:49:28 +0000

    added more tests for dual classification and hatched zone classification

--- tests/test_uscs_aashto.py
+++ tests/test_uscs_aashto.py
@@ -41,9 +41,23 @@ def test_PI():
 def test_dual_classification():
     soil = Soil(30.8, 20.7, 10.1, 10.29, 81.89, 7.83, 0.07, 0.3, 0.8)
     assert soil.unified_classification == "SW-SC"
+    assert soil.cu == pytest.approx(11.43, 0.01)
+    assert soil.cc == pytest.approx(1.61, 0.01)
 
     soil = Soil(24.4, 14.7, 9.7, 9.77, 44.82, 45.41, 0.06, 0.6, 7)
     assert soil.unified_classification == "GP-GC"
+    assert soil.cu == pytest.approx(116.67, 0.01)
+    assert soil.cc == pytest.approx(0.86, 0.01)
+
+    soil = Soil(49.5, 33.6, 15.9, 6.93, 91.79, 1.28, 0.153, 0.4, 1.2)
+    assert soil.unified_classification == "SP-SM"
+    assert soil.cu == pytest.approx(7.84, 0.01)
+    assert soil.cc == pytest.approx(0.87, 0.01)
+
+
+def test_soil_in_hatched_zone():
+    soil = Soil(26.4, 19.4, 7, 54.76, 45.24, 0)
+    assert soil.unified_classification == "ML-CL"
 
 
 def _get_params(soils):
