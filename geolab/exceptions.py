12f8cad71a75f1c9a8c87006e7dcac7c428bfda1
Pato <boatengpato.pb@gmail.com>
2023-05-13 02:27:33 +0000

    added error

--- geolab/exceptions.py
+++ geolab/exceptions.py
@@ -12,16 +12,12 @@ class PIValueError(ArithmeticError):
     """
 
 
-class SoilTypeError(TypeError):
-    """Exception raised when an invalid soil type is specified."""
-
-
 class FoundationTypeError(TypeError):
     """Exception raised when an invalid foundation type is specified."""
 
 
-class SPTCorrectionTypeError(TypeError):
-    """Exception raised when an invalid spt correction type is specified."""
+class SoilTypeError(TypeError):
+    """Exception raised when an invalid soil type is specified."""
 
 
 class AllowableSettlementError(ValueError):
