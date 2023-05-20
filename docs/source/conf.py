370210d432ecf2b51eb91e3b4f935d802d3b471f
Pato <boatengpato.pb@gmail.com>
2023-05-20 12:27:56 +0000

    changed html_theme to pydata_sphinx_theme, and added a custom css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,5 +23,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
