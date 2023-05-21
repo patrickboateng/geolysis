ba73ce7d66f6ee22b6fd1fd26e883e6bce5ac0e5
Pato <boatengpato.pb@gmail.com>
2023-05-21 20:51:56 +0000

    changed docstyle to rst

--- geolab/__init__.py
+++ geolab/__init__.py
@@ -1,27 +1,32 @@
 """
-# geolab
+geolab
+======
 
-geolab is an open-source software that provides tools for geotechnical engineering,
+``geolab`` is an open-source software that provides tools for geotechnical engineering,
 including soil classification, bearing capacity analysis, estimation of soil engineering properties,
 settlement analysis, and finite element modelling. It helps engineers work more efficiently and make
 informed decisions about design and construction.
 
-## Soil Parameter Estimators
+Soil Parameter Estimators
+-------------------------
 
 
-## Bearing Capacity Analysis
+Bearing Capacity Analysis
+-------------------------
 
 
-## Exceptions
+Exceptions
+----------
 
 """
 
 import numpy as np
 
-from .exceptions import PIValueError, PSDValueError
 from geolab.utils import deg2rad
 
-VERSION = "0.1.0"
+from .exceptions import PIValueError, PSDValueError
+
+__version__ = "0.1.0"
 ERROR_TOLERANCE = 0.01
 DECIMAL_PLACES = 2
 REFERENCES = {
@@ -41,14 +46,15 @@ REFERENCES = {
 
 @deg2rad("friction_angle")
 def passive_earth_pressure_coef(*, friction_angle: float) -> float:
-    r"""Coefficient of passive earth pressure ($K_p$).
+    r"""Coefficient of passive earth pressure :math:`K_p`.
 
-    $$\dfrac{1 + \sin \phi}{1 - \sin \phi}$$
+    .. math::
 
-    Args:
-        friction_angle: Internal angle of friction (degrees).
+        \dfrac{1 + \sin \phi}{1 - \sin \phi}
 
-    Returns:
-        Passive earth pressure coefficient.
+    :param friction_angle: Internal angle of friction (degrees)
+    :type friction_angle: float
+    :return: Passive earth pressure coefficient
+    :rtype: float
     """
     return (1 + np.sin(friction_angle)) / (1 - np.sin(friction_angle))
