16254dec0891d249b44a60af331624d90c59ff56
Pato <boatengpato.pb@gmail.com>
2023-05-29 21:27:05 +0000

    earth pressure theories

---
+++ geolab/earth_pressure.py
@@ -0,0 +1,20 @@
+from geolab import DECIMAL_PLACES
+from geolab.utils import deg2rad, sin
+
+
+@deg2rad
+def passive_earth_pressure_coef(*, friction_angle: float) -> float:
+    r"""Coefficient of passive earth pressure :math:`K_p`.
+
+    .. math::
+
+        \dfrac{1 + \sin \phi}{1 - \sin \phi}
+
+    :param friction_angle: Internal angle of friction (degrees)
+    :type friction_angle: float
+    :return: Passive earth pressure coefficient
+    :rtype: float
+    """
+    phi = friction_angle
+    kp = (1 + sin(phi)) / (1 - sin(phi))
+    return round(kp, DECIMAL_PLACES)
