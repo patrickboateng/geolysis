f93ea651ed2afe1ef91bbc17226ab064f8031821
Pato <boatengpato.pb@gmail.com>
2023-05-30 00:47:04 +0000

    removed passive_earth_pressure_coef function

--- geolab/__init__.py
+++ geolab/__init__.py
@@ -29,20 +29,3 @@ from .exceptions import PIValueError, PSDValueError
 __version__ = "0.1.0"
 ERROR_TOLERANCE = 0.01
 DECIMAL_PLACES = 2
-
-
-@deg2rad
-def passive_earth_pressure_coef(*, friction_angle: float) -> float:
-    r"""Coefficient of passive earth pressure :math:`K_p`.
-
-    .. math::
-
-        \dfrac{1 + \sin \phi}{1 - \sin \phi}
-
-    :param friction_angle: Internal angle of friction (degrees)
-    :type friction_angle: float
-    :return: Passive earth pressure coefficient
-    :rtype: float
-    """
-    kp = (1 + np.sin(friction_angle)) / (1 - np.sin(friction_angle))
-    return round(kp, DECIMAL_PLACES)
