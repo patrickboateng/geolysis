0c1f61d926fa15d7b7423a661cc29fbefdb7b177
Pato <boatengpato.pb@gmail.com>
2023-06-06 20:13:00 +0000

    added tests for spt n60 corrector

--- tests/test_corrections.py
+++ tests/test_corrections.py
@@ -1,7 +1,7 @@
 import pytest
 
 from geolab import ERROR_TOLERANCE
-from geolab.corrections import dilatancy_spt_correction
+from geolab.bearing_capacity.spt_corrections import dilatancy_spt_correction, spt_n60
 
 
 @pytest.mark.parametrize(
@@ -9,3 +9,7 @@ from geolab.corrections import dilatancy_spt_correction
 )
 def test_dilatancy_spt_correction(n_value, exp):
     assert dilatancy_spt_correction(n_value) == pytest.approx(exp, ERROR_TOLERANCE)
+
+
+def test_spt_n60():
+    assert spt_n60(15, 0.6, 1.0, 1.0, 0.85) == pytest.approx(12.75, ERROR_TOLERANCE)
