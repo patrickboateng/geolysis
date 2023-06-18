adda3f1fda02164f4786b96a5087caf4073666e9
Pato <boatengpato.pb@gmail.com>
2023-06-18 11:34:16 +0000

    refactored functions

--- geolab/utils.py
+++ geolab/utils.py
@@ -1,31 +1,46 @@
 import math
 
-import numpy as np
 
-exp = np.exp
-pi = np.pi
+def deg2rad(x: float) -> float:
+    return math.radians(x)
 
-deg2rad = lambda x: np.deg2rad(x)
 
-rad2deg = lambda x: np.rad2deg(x)
+def rad2deg(x: float) -> float:
+    return math.degrees(x)
 
-tan = lambda x: np.tan(deg2rad(x))
 
-sin = lambda x: np.sin(deg2rad(x))
+def tan(x: float) -> float:
+    return math.tan(deg2rad(x))
 
-cos = lambda x: np.cos(deg2rad(x))
 
-arctan = lambda x: rad2deg(np.arctan(x))
+def sin(x: float) -> float:
+    return math.sin(deg2rad(x))
 
-log10 = lambda x: np.log10(x)
 
-sqrt = lambda x: np.sqrt(x)
+def cos(x: float) -> float:
+    return math.cos(deg2rad(x))
+
+
+def arctan(x: float) -> float:
+    return rad2deg(math.atan(x))
+
+
+def log10(x: float) -> float:
+    return math.log10(x)
+
+
+def sqrt(x: float) -> float:
+    return math.sqrt(x)
+
+
+exp = math.exp
+pi = math.pi
 
 
 def mul(*args) -> float:
-	"""Calculate the product of all the elements in the input iterable.
+    """Calculate the product of all the elements in the input iterable.
 
     :return: The products of all elements in `args`
     :rtype: float
     """
-	return math.prod(args)
+    return math.prod(args)
