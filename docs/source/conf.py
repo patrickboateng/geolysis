d906a90db4897e78adde48852021e0100174d6fe
Pato <boatengpato.pb@gmail.com>
2023-08-04 16:42:49 +0000

    added more extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,13 @@ release = "0.1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.doctest", "sphinxcontrib.bibtex", "sphinx.ext.autodoc"]
+extensions = [
+    "sphinx.ext.doctest",
+    "sphinxcontrib.bibtex",
+    "sphinx.ext.autodoc",
+    "sphinx_copybutton",
+    "sphinx_design",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = []
