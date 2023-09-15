ac20686a9f3c2de2c79a09b4e5eb4e36b184d6e4
Pato <boatengpato.pb@gmail.com>
2023-09-15 12:27:53 +0000

    modified function name

--- tests/test_estimators.py
+++ tests/test_estimators.py
@@ -1,10 +1,12 @@
 import pytest
 
 from geolab import ERROR_TOLERANCE
-from geolab.estimators import misc
+from geolab.estimators import rankine_foundation_depth
 
 
 def test_foundation_depth():
-    assert misc.foundation_depth(350, 18, friction_angle=35) == pytest.approx(
-        1.429, ERROR_TOLERANCE
+    assert rankine_foundation_depth(
+        350, 18, friction_angle=35
+    ) == pytest.approx(  # type: ignore
+        1.4, ERROR_TOLERANCE
     )
