df151b30e2b8f4bb14dbe59b5fc3daccca16c2b0
Pato <boatengpato.pb@gmail.com>
2023-09-15 13:42:55 +0000

    minor testing

--- geolab/main.py
+++ geolab/main.py
@@ -2,9 +2,18 @@ import sys
 
 sys.path.insert(0, ".")
 
-from geolab.bearing_capacity.meyerhof import meyerhoff_bearing_capacity
+from geolab.bearing_capacity import FootingSize, FoundationSize
+from geolab.bearing_capacity.ultimate import terzaghi_bearing_capacity
+from geolab.utils import arctan, cos, isclose, log10, sin
 
 if __name__ == "__main__":
-    m = meyerhoff_bearing_capacity([1, 2, 3, 4, 5])
+    # spt = spt_corrections().n_design([1, 2, 4, 5])
+    # print(spt)
+    # print(cos(115))
+    # print(arctan(45))
+    f_size = FootingSize(1.715, 1.715)
+    fs = FoundationSize(f_size, 1.2)
+    t = terzaghi_bearing_capacity(10.7, 18.76, 18.5, fs)
 
-    print(m.n_design)
+    print(t.ultimate_4_square_footing())
+    print(t.nc, t.nq, t.ngamma)
