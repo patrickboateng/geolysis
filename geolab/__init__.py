40894f2ff1f0808a007f22e27bca87d05fe71eb2
Pato <boatengpato.pb@gmail.com>
2023-09-17 11:18:25 +0000

    added docstring to enum

--- geolab/__init__.py
+++ geolab/__init__.py
@@ -1,5 +1,3 @@
-""""""
-
 import enum
 
 __version__ = "0.1.0"
@@ -8,6 +6,8 @@ DECIMAL_PLACES = 3
 
 
 class GeotechEng(enum.IntEnum):
+    """Represents a set of ``Geotechnical Engineers``."""
+
     BAZARAA = enum.auto()
     GIBBS = enum.auto()
     HANSEN = enum.auto()
@@ -25,4 +25,7 @@ class GeotechEng(enum.IntEnum):
         return self.name
 
 
+# Make it possible to write geolab.BAZARAA after import geolab
+# import geolab
+# geolab.BAZARAA is GeotechEng.BAZARAA returns True
 globals().update(GeotechEng.__members__)
