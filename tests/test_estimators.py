51a9e48848712ede7c4beb51a403478a84c55f26
Pato <boatengpato.pb@gmail.com>
2023-09-17 17:52:56 +0000

    added tests for soil_unit_weight

--- tests/test_estimators.py
+++ tests/test_estimators.py
@@ -1,7 +1,14 @@
 import pytest
 
 from geolab import ERROR_TOLERANCE
-from geolab.estimators import rankine_foundation_depth
+from geolab.estimators import rankine_foundation_depth, soil_unit_weight
+
+
+def test_soil_unit_weight():
+    suw = soil_unit_weight(spt_n60=13)
+    assert suw.moist == pytest.approx(17.3, ERROR_TOLERANCE)
+    assert suw.saturated == pytest.approx(18.75, ERROR_TOLERANCE)
+    assert suw.submerged == pytest.approx(8.93, ERROR_TOLERANCE)
 
 
 def test_foundation_depth():
