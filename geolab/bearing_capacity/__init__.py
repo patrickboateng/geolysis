9a8916ac043be39e6b05de968989aab93237eea0
Pato <boatengpato.pb@gmail.com>
2023-09-17 01:14:04 +0000

    added a method

--- geolab/bearing_capacity/__init__.py
+++ geolab/bearing_capacity/__init__.py
@@ -9,6 +9,10 @@ class FootingSize:
     width: float
     length: float
 
+    @property
+    def width_2_length_ratio(self) -> float:
+        return self.width / self.length
+
 
 @dataclass(slots=True)
 class FoundationSize:
@@ -26,6 +30,10 @@ class FoundationSize:
     @property
     def depth_2_width_ratio(self) -> float:
         return self.depth / self.width
+
+    @property
+    def width_2_length_ratio(self) -> float:
+        return self.footing_size.width_2_length_ratio
 
 
 class FootingShape(enum.IntEnum):
