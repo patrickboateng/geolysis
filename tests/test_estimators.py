737814ed99ff28d1407bccc8f0100dc6e15cd563
Pato <boatengpato.pb@gmail.com>
2023-09-18 19:08:41 +0000

    wrote tests for soil_friction_angle

--- tests/test_estimators.py
+++ tests/test_estimators.py
@@ -1,7 +1,44 @@
 import pytest
 
-from geolab import ERROR_TOLERANCE
-from geolab.estimators import rankine_foundation_depth, soil_unit_weight
+from geolab import ERROR_TOLERANCE, GeotechEng
+from geolab.estimators import (
+    rankine_foundation_depth,
+    soil_friction_angle,
+    soil_unit_weight,
+)
+from geolab.exceptions import EngineerTypeError
+
+
+def test_foundation_depth():
+    assert rankine_foundation_depth(
+        350, 18, friction_angle=35
+    ) == pytest.approx(1.4, ERROR_TOLERANCE)
+
+
+def test_soil_friction_angle():
+    sfa = soil_friction_angle(spt_n60=50)
+    exp = 40.75
+    assert sfa() == pytest.approx(exp, ERROR_TOLERANCE)
+    assert sfa.wolff_1989() == pytest.approx(exp, ERROR_TOLERANCE)
+
+    sfa.spt_n60 = 40
+    exp = 38.236
+    assert sfa() == pytest.approx(exp, ERROR_TOLERANCE)
+    assert sfa.wolff_1989() == pytest.approx(exp, ERROR_TOLERANCE)
+
+    sfa = soil_friction_angle(
+        spt_n60=40,
+        eop=103.8,
+        atm_pressure=101.325,
+        eng=GeotechEng.KULLHAWY,
+    )
+    exp = 46.874
+    assert sfa() == pytest.approx(exp, ERROR_TOLERANCE)
+    assert sfa.kullhawy_mayne_1990() == pytest.approx(exp, ERROR_TOLERANCE)
+
+    with pytest.raises(EngineerTypeError):
+        sfa = soil_friction_angle(spt_n60=35, eng=GeotechEng.BAZARAA)
+        sfa()
 
 
 def test_soil_unit_weight():
@@ -9,11 +46,3 @@ def test_soil_unit_weight():
     assert suw.moist == pytest.approx(17.3, ERROR_TOLERANCE)
     assert suw.saturated == pytest.approx(18.75, ERROR_TOLERANCE)
     assert suw.submerged == pytest.approx(8.93, ERROR_TOLERANCE)
-
-
-def test_foundation_depth():
-    assert rankine_foundation_depth(
-        350, 18, friction_angle=35
-    ) == pytest.approx(  # type: ignore
-        1.4, ERROR_TOLERANCE
-    )
