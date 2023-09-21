42f9af3b089c5c4218925a8f8b15a818508d4c53
Pato <boatengpato.pb@gmail.com>
2023-09-21 10:01:04 +0000

    removed unused exception

--- geolab/exceptions.py
+++ geolab/exceptions.py
@@ -7,10 +7,6 @@ class PIValueError(ArithmeticError):
     """Exception raised when ``PI`` is not equal to ``LL - PL``."""
 
 
-class SoilTypeError(TypeError):
-    """Exception raised when an invalid soil type is specified."""
-
-
 class AllowableSettlementError(ValueError):
     """Exception raised when allowable settlement is greater than ``25.4mm``."""
 
