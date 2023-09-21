f9b155926fa766a8f7515f514db40b87145a1473
Pato <boatengpato.pb@gmail.com>
2023-09-21 14:23:42 +0000

    reformatted docstring

--- geolab/exceptions.py
+++ geolab/exceptions.py
@@ -1,15 +1,23 @@
 class PSDValueError(ArithmeticError):
-    """Exception raised when soil aggregates does not approximately
-    sum to 100%."""
+    """
+    Exception raised when soil aggregates does not approximately
+    sum to 100%.
+    """
 
 
 class PIValueError(ArithmeticError):
-    """Exception raised when ``PI`` is not equal to ``LL - PL``."""
+    """
+    Exception raised when ``PI`` is not equal to ``LL - PL``.
+    """
 
 
 class AllowableSettlementError(ValueError):
-    """Exception raised when allowable settlement is greater than ``25.4mm``."""
+    """
+    Exception raised when allowable settlement is greater than ``25.4mm``.
+    """
 
 
 class EngineerTypeError(TypeError):
-    """Exception raise when a particular engineer is not allowed"""
+    """
+    Exception raise when a particular engineer is not allowed.
+    """
