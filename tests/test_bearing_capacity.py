165123288cd6e6e4fe5493d26614857247103e98
Patrick Boateng <boatengpato.pb@gmail.com>
2023-10-28 21:42:36 +0000

    redesigning tests

--- tests/test_bearing_capacity.py
+++ tests/test_bearing_capacity.py
@@ -2,10 +2,10 @@ import pytest
 
 from geolysis import ERROR_TOLERANCE
 from geolysis.bearing_capacity import FoundationSize
-from geolysis.bearing_capacity.hansen import HansenBearingCapacityFactors
+from geolysis.bearing_capacity.hansen import HansenFactors
 from geolysis.bearing_capacity.terzaghi import (
     TerzaghiBearingCapacity,
-    TerzaghiBearingCapacityFactors,
+    TerzaghiFactors,
 )
 from geolysis.bearing_capacity.vesic import VesicBearingCapacityFactors
 
@@ -74,39 +74,39 @@ class TestTerzaghiBearingCapacity:
     ],
 )
 def test_terzaghi_bcf(soil_friction_angle: float, bcf: dict):
-    tbcf = TerzaghiBearingCapacityFactors(soil_friction_angle)
+    tbcf = TerzaghiFactors(soil_friction_angle)
     assert tbcf.nc == pytest.approx(bcf["nc"], ERROR_TOLERANCE)
     assert tbcf.nq == pytest.approx(bcf["nq"], ERROR_TOLERANCE)
     assert tbcf.ngamma == pytest.approx(bcf["ngamma"], ERROR_TOLERANCE)
 
 
-@pytest.mark.parametrize(
-    "soil_friction_angle,bcf",
-    [
-        (5, {"nq": 1.57, "nc": 6.49, "ngamma": 0.09}),
-        (10, {"nq": 2.47, "nc": 8.35, "ngamma": 0.47}),
-        (15, {"nq": 3.94, "nc": 10.98, "ngamma": 1.42}),
-        (20, {"nq": 6.40, "nc": 14.83, "ngamma": 3.54}),
-    ],
-)
-def test_hansen_bcf(soil_friction_angle: float, bcf: dict):
-    hbcf = HansenBearingCapacityFactors(soil_friction_angle)
-    assert hbcf.nc == pytest.approx(bcf["nc"], ERROR_TOLERANCE)
-    assert hbcf.nq == pytest.approx(bcf["nq"], ERROR_TOLERANCE)
-    assert hbcf.ngamma == pytest.approx(bcf["ngamma"], ERROR_TOLERANCE)
+# @pytest.mark.parametrize(
+#     "soil_friction_angle,bcf",
+#     [
+#         (5, {"nq": 1.57, "nc": 6.49, "ngamma": 0.09}),
+#         (10, {"nq": 2.47, "nc": 8.35, "ngamma": 0.47}),
+#         (15, {"nq": 3.94, "nc": 10.98, "ngamma": 1.42}),
+#         (20, {"nq": 6.40, "nc": 14.83, "ngamma": 3.54}),
+#     ],
+# )
+# def test_hansen_bcf(soil_friction_angle: float, bcf: dict):
+#     hbcf = HansenFactors(soil_friction_angle)
+#     assert hbcf.nc == pytest.approx(bcf["nc"], ERROR_TOLERANCE)
+#     assert hbcf.nq == pytest.approx(bcf["nq"], ERROR_TOLERANCE)
+#     assert hbcf.ngamma == pytest.approx(bcf["ngamma"], ERROR_TOLERANCE)
 
 
-@pytest.mark.parametrize(
-    "soil_friction_angle,bcf",
-    [
-        (5, {"nq": 1.57, "nc": 6.49, "ngamma": 0.45}),
-        (10, {"nq": 2.47, "nc": 8.35, "ngamma": 1.22}),
-        (15, {"nq": 3.94, "nc": 10.98, "ngamma": 2.65}),
-        (20, {"nq": 6.40, "nc": 14.83, "ngamma": 5.39}),
-    ],
-)
-def test_vesic_bcf(soil_friction_angle: float, bcf: dict):
-    vbcf = VesicBearingCapacityFactors(soil_friction_angle)
-    assert vbcf.nc == pytest.approx(bcf["nc"], ERROR_TOLERANCE)
-    assert vbcf.nq == pytest.approx(bcf["nq"], ERROR_TOLERANCE)
-    assert vbcf.ngamma == pytest.approx(bcf["ngamma"], ERROR_TOLERANCE)
+# @pytest.mark.parametrize(
+#     "soil_friction_angle,bcf",
+#     [
+#         (5, {"nq": 1.57, "nc": 6.49, "ngamma": 0.45}),
+#         (10, {"nq": 2.47, "nc": 8.35, "ngamma": 1.22}),
+#         (15, {"nq": 3.94, "nc": 10.98, "ngamma": 2.65}),
+#         (20, {"nq": 6.40, "nc": 14.83, "ngamma": 5.39}),
+#     ],
+# )
+# def test_vesic_bcf(soil_friction_angle: float, bcf: dict):
+#     vbcf = VesicBearingCapacityFactors(soil_friction_angle)
+#     assert vbcf.nc == pytest.approx(bcf["nc"], ERROR_TOLERANCE)
+#     assert vbcf.nq == pytest.approx(bcf["nq"], ERROR_TOLERANCE)
+#     assert vbcf.ngamma == pytest.approx(bcf["ngamma"], ERROR_TOLERANCE)
