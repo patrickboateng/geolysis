088989c7b3935216bc50ef7728c1e107cb76716e
Patrick Boateng <boatengpato.pb@gmail.com>
2023-11-05 20:22:46 +0000

    fixed failing tests

--- tests/test_estimators.py
+++ tests/test_estimators.py
@@ -1,6 +1,6 @@
 import pytest
 
-from geolysis import ERROR_TOLERANCE, GeotechEng
+from geolysis import ERROR_TOLERANCE
 from geolysis.estimators import (
     CompressionIndex,
     SoilFrictionAngle,
@@ -8,26 +8,17 @@ from geolysis.estimators import (
     UndrainedShearStrength,
     rankine_foundation_depth,
 )
-from geolysis.exceptions import EngineerTypeError
 
 
 def test_compression_index():
-    compression_index = CompressionIndex(liquid_limit=35)
-    assert compression_index.skempton_1994() == pytest.approx(
-        0.175, ERROR_TOLERANCE
-    )
-    assert compression_index.terzaghi_et_al_1967() == pytest.approx(
+    comp_idx = CompressionIndex(liquid_limit=35)
+    assert comp_idx.skempton_1994() == pytest.approx(0.175, ERROR_TOLERANCE)
+    assert comp_idx.terzaghi_et_al_1967() == pytest.approx(
         0.225, ERROR_TOLERANCE
     )
 
-    compression_index = CompressionIndex(void_ratio=0.78, eng=GeotechEng.HOUGH)
-    assert compression_index.hough_1957() == pytest.approx(
-        0.148, ERROR_TOLERANCE
-    )
-
-    with pytest.raises(EngineerTypeError):
-        compression_index = CompressionIndex(eng=GeotechEng.GIBBS)
-        compression_index()
+    comp_idx = CompressionIndex(void_ratio=0.78)
+    assert comp_idx.hough_1957() == pytest.approx(0.148, ERROR_TOLERANCE)
 
 
 def test_soil_friction_angle():
@@ -37,17 +28,8 @@ def test_soil_friction_angle():
     sfa.spt_n60 = 40
     assert sfa.wolff_1989() == pytest.approx(38.236, ERROR_TOLERANCE)
 
-    sfa = SoilFrictionAngle(
-        spt_n60=40,
-        eop=103.8,
-        atm_pressure=101.325,
-        eng=GeotechEng.KULLHAWY,
-    )
+    sfa = SoilFrictionAngle(spt_n60=40, eop=103.8, atm_pressure=101.325)
     assert sfa.kullhawy_mayne_1990() == pytest.approx(46.874, ERROR_TOLERANCE)
-
-    with pytest.raises(EngineerTypeError):
-        sfa = SoilFrictionAngle(spt_n60=35, eng=GeotechEng.BAZARAA)
-        sfa()
 
 
 def test_soil_unit_weight():
@@ -61,20 +43,14 @@ def test_undrained_shear_strength():
     uss = UndrainedShearStrength(spt_n60=40)
     assert uss.stroud_1974() == pytest.approx(140, ERROR_TOLERANCE)
 
-    uss = UndrainedShearStrength(
-        spt_n60=40,
-        eop=108.3,
-        plasticity_index=12,
-        eng=GeotechEng.SKEMPTON,
-    )
+    uss = UndrainedShearStrength(spt_n60=40, eop=108.3, plasticity_index=12)
     assert uss.skempton_1957() == pytest.approx(16.722, ERROR_TOLERANCE)
-
-    with pytest.raises(EngineerTypeError):
-        uss = UndrainedShearStrength(spt_n60=30, eng=GeotechEng.LIAO)
-        uss()
 
 
 def test_foundation_depth():
-    assert rankine_foundation_depth(
-        350, 18, soil_friction_angle=35
-    ) == pytest.approx(1.4, ERROR_TOLERANCE)
+    est_depth = rankine_foundation_depth(
+        allowable_bearing_capacity=350,
+        soil_unit_weight=18,
+        soil_friction_angle=35,
+    )
+    assert est_depth == pytest.approx(1.4, ERROR_TOLERANCE)
