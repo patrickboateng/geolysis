b23aebc257402d096cf1557fc72fd4ead406abb8
Patrick Boateng <boatengpato.pb@gmail.com>
2023-12-24 21:35:35 +0000

    added a foundation size fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,12 @@
+import pytest
 
+from geolysis.bearing_capacity import FoundationSize
+
+
+@pytest.fixture
+def fs():
+    return FoundationSize(
+        foundation_depth=1.5,
+        footing_length=1.2,
+        footing_width=1.2,
+    )
