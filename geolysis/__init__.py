5b03ddd41aca38a0c93353827ccb1b97976d46c2
Patrick Boateng <boatengpato.pb@gmail.com>
2023-12-26 13:31:38 +0000

    changed enum class from IntFlag to IntEnum

--- geolysis/__init__.py
+++ geolysis/__init__.py
@@ -2,10 +2,9 @@ import enum
 
 __version__ = "0.1.8"
 ERROR_TOLERANCE = 0.01
-DECIMAL_PLACES = 3
 
 
-class GeotechEng(enum.IntFlag):
+class GeotechEng(enum.IntEnum):
     """Represents a set of ``Geotechnical Engineers``."""
 
     BAZARAA = 1
@@ -21,12 +20,6 @@ class GeotechEng(enum.IntFlag):
     VESIC = 11
     WOLFF = 12
     PECK = 13
-
-    def __str__(self) -> str:
-        return self.name
-
-    def __repr__(self) -> str:
-        return self.__str__()
 
 
 # Make it possible to write geolab.BAZARAA after importing
