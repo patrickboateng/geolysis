60c6af0f6690e8e713e44f3f6c359b5cf4f98023
Patrick Boateng <boatengpato.pb@gmail.com>
2023-12-28 19:40:41 +0000

    redesigned module

--- geolysis/estimators.py
+++ geolysis/estimators.py
@@ -2,218 +2,202 @@
 estimators.
 """
 
-from geolysis.utils import arctan, round_, sin
+from geolysis.utils import arctan, round_
 
 
 class SoilUnitWeight:
     """Calculates the ``moist``, ``saturated`` and ``submerged`` unit weight
-    of soil sample using ``SPT N60``.
+    of soil sample using
+    :class:`SPT N60 <geolysis.bearing_capacity.spt.SPTCorrection.spt_n60>`.
 
     :param float spt_n60:
         SPT N-value standardized for field procedures.
     """
 
-    __slots__ = ("spt_n60",)
+    __slots__ = ("spt_n_60",)
 
-    def __init__(self, spt_n60: float):
-        self.spt_n60 = spt_n60
+    def __init__(self, spt_n_60: float):
+        self.spt_n_60 = spt_n_60
 
     @property
     @round_
-    def moist(self) -> float:
+    def est_moist_wgt(self) -> float:
         r"""Return the moist unit weight for cohesionless soil."""
-        return 16.0 + 0.1 * self.spt_n60
+        return 16.0 + 0.1 * self.spt_n_60
 
     @property
     @round_
-    def saturated(self) -> float:
+    def est_saturated_wgt(self) -> float:
         r"""Return the saturated unit weight for cohesive soil."""
-        return 16.8 + 0.15 * self.spt_n60
+        return 16.8 + 0.15 * self.spt_n_60
 
     @property
     @round_
-    def submerged(self) -> float:
+    def est_submerged_wgt(self) -> float:
         r"""Return the submerged unit weight of cohesionless soil."""
-        return 8.8 + 0.01 * self.spt_n60
+        return 8.8 + 0.01 * self.spt_n_60
 
 
-class CompressionIndex:
-    r"""The compression index of soil estimated from ``liquid limit``
+class CompressionIndexEst:
+    """The compression index of soil estimated from ``liquid limit``
     or ``void ratio``.
 
     The available correlations used are  :meth:`SKEMPTON (1994) <.skempton_1994>`,
-    :meth:`TERZAGHI ET AL (1967) <.terzaghi_et_al_1967`,
-    :meth:`HOUGH (1957) <.hough_1957`.
-
-    :param float liquid_limit:
-        Water content beyond which soils flows under their own weight (%)
-    :param float void_ratio:
-        Ratio of the volume of voids to the volume of solids (unitless)
+    :meth:`TERZAGHI ET AL (1967) <.terzaghi_et_al_1967>`,
+    :meth:`HOUGH (1957) <.hough_1957>`.
     """
-    __slots__ = ("liquid_limit", "void_ratio")
 
-    def __init__(
-        self,
-        *,
-        liquid_limit: float = 0.0,
-        void_ratio: float = 0.0,
-    ):
-        self.liquid_limit = liquid_limit
-        self.void_ratio = void_ratio
-
+    @staticmethod
     @round_(ndigits=3)
-    def terzaghi_et_al_1967(self) -> float:
+    def terzaghi_et_al_1967(liquid_limit: float) -> float:
         r"""Return the compression index of the soil using ``Terzaghi's``
         correlation.
-        """
-        return 0.009 * (self.liquid_limit - 10)
 
+        :param float liquid_limit:
+            Water content beyond which soils flows under their own weight (%)
+        """
+        return 0.009 * (liquid_limit - 10)
+
+    @staticmethod
     @round_(ndigits=3)
-    def skempton_1994(self) -> float:
+    def skempton_1994(liquid_limit: float) -> float:
         r"""Return the compression index of the soil using ``Skempton's``
         correlation.
-        """
-        return 0.007 * (self.liquid_limit - 10)
 
+        :param float liquid_limit:
+            Water content beyond which soils flows under their own weight (%)
+        """
+        return 0.007 * (liquid_limit - 10)
+
+    @staticmethod
     @round_(ndigits=3)
-    def hough_1957(self) -> float:
+    def hough_1957(void_ratio: float) -> float:
         r"""Return the compression index of the soil using ``Hough's``
         correlation.
+
+        :param float void_ratio:
+            Ratio of the volume of voids to the volume of solids (unitless)
         """
-        return 0.29 * (self.void_ratio - 0.27)
+        return 0.29 * (void_ratio - 0.27)
 
 
-class SoilFrictionAngle:
+class SoilFrictionAngleEst:
     r"""Estimation of the internal angle of friction using ``SPT N60``.
 
     For cohesionless soils the coefficient of internal friction (:math:`\phi`)
     was determined from the minimum value from :meth:`WOLFF (1989) <.wolff_1989>`
     and :meth:`KULLHAWY & MAYNE (1990) <.kullhawy_mayne_1990>`.
-
-    :param float spt_n60:
-        SPT N-value standardized for field procedures.
-    :param float eop:
-        Effective overburden pressure (:math:`kN/m^2`), defaults to 0
-    :param float atm_pressure:
-        Atmospheric pressure (:math:`kN/m^2`), defaults to 0
     """
-    __slots__ = ("spt_n60", "eop", "atm_pressure")
 
-    def __init__(
-        self,
-        *,
-        spt_n60: float,
-        eop: float = 0,
-        atm_pressure: float = 0,
-    ):
-        self.spt_n60 = spt_n60
-        self.eop = eop
-        self.atm_pressure = atm_pressure
-
+    @staticmethod
     @round_
-    def wolff_1989(self) -> float:
-        r"""Return the internal angle of friction using ``Wolff's`` correlation
+    def wolff_1989(spt_n_60: float) -> float:
+        """Return the internal angle of friction using ``Wolff's`` correlation
         for granular soils (degrees).
-        """
-        return 27.1 + (0.3 * self.spt_n60) - (0.00054 * (self.spt_n60**2))
 
+        :param float spt_n_60:
+            SPT N-value standardized for field procedures.
+        """
+        return 27.1 + (0.3 * spt_n_60) - (0.00054 * (spt_n_60**2))
+
+    @staticmethod
     @round_
-    def kullhawy_mayne_1990(self) -> float:
-        r"""Return the internal angle of friction using ``Kullhawy & Mayne``
+    def kullhawy_mayne_1990(
+        spt_n_60: float,
+        eop: float,
+        atm_pressure: float,
+    ) -> float:
+        """Return the internal angle of friction using ``Kullhawy & Mayne``
         correlation for cohesionless soils (degrees).
+
+        :param float spt_n_60:
+            SPT N-value standardized for field procedures.
+        :param float eop:
+            Effective overburden pressure (:math:`kN/m^2`), defaults to 0
+        :param float atm_pressure:
+            Atmospheric pressure (:math:`kN/m^2`), defaults to 0
         """
-        expr = self.spt_n60 / (12.2 + 20.3 * (self.eop / self.atm_pressure))
-        return arctan(expr**0.34)
+        return arctan(
+            (spt_n_60 / (12.2 + 20.3 * (eop / atm_pressure))) ** 0.34
+        )
 
 
-class UndrainedShearStrength:
+class UndrainedShearStrengthEst:
     r"""Undrained shear strength of soil.
 
     The available correlations used are :meth:`STROUD (1974) <.stroud_1974>`
     and :meth:`SKEMPTON (1957) <.skempton_1957>`.
-
-    :param float spt_n60:
-        SPT N-value corrected for 60% hammer efficiency, defaults to 0
-    :param float eop:
-        Effective overburden pressure :math:`kN/m^2`, defaults to 0
-    :param float plasticity_index:
-        Range of water content over which soil remains in plastic condition,
-        defaults to 0
-    :param float k:
-        stroud parameter, defaults to 3.5
     """
 
-    __slots__ = ("spt_n60", "eop", "plasticity_index", "k")
-
-    def __init__(
-        self,
-        *,
-        spt_n60: float = 0,
-        eop: float = 0,
-        plasticity_index: float = 0,
-        k: float = 3.5,
-    ) -> None:
-        self.spt_n60 = spt_n60
-        self.eop = eop
-        self.plasticity_index = plasticity_index
-        self.k = k
-
+    @staticmethod
     @round_
-    def stroud_1974(self):
+    def stroud_1974(spt_n_60: float, k=3.5):
         r"""Return the undrained shear strength using ``Stroud's`` correlation.
+
+        :param float spt_n60:
+            SPT N-value standardized for field procedures.
+        :param float k:
+            stroud parameter, defaults to 3.5
 
         :raises ValueError: If ``k`` is not in the specified range.
         """
-        if 3.5 <= self.k <= 6.5:
-            return self.k * self.spt_n60
+        if 3.5 <= k <= 6.5:
+            return k * spt_n_60
 
-        msg = f"k should be in the range 3.5 <= k <= 6.5 not {self.k}"
+        msg = f"k should be in the range 3.5 <= k <= 6.5 not {k}"
         raise ValueError(msg)
 
+    @staticmethod
     @round_
-    def skempton_1957(self):
+    def skempton_1957(eop: float, plasticity_index: float):
         r"""Return the undrained shear strength using ``Skempton's``
         correlation.
+
+        :param float eop:
+            Effective overburden pressure :math:`kN/m^2`, defaults to 0
+        :param float plasticity_index:
+            Range of water content over which soil remains in plastic condition,
+            defaults to 0
         """
-        return self.eop * (0.11 + 0.0037 * self.plasticity_index)
+        return eop * (0.11 + 0.0037 * plasticity_index)
 
 
-@round_(ndigits=2)
-def bowles_est_soil_elastic_modulus(spt_n60: float) -> float:
-    r"""Return elastic modulus of soil estimated from ``Joseph Bowles``
-    correlation.
+# @round_(ndigits=2)
+# def bowles_est_soil_elastic_modulus(spt_n60: float) -> float:
+#     r"""Return elastic modulus of soil estimated from ``Joseph Bowles``
+#     correlation.
 
-    :param float spt_n60:
-        SPT N-value standardized for field procedures.
+#     :param float spt_n60:
+#         SPT N-value standardized for field procedures.
 
-    :return:
-        Elastic modulus of soil (:math:`kN/m^2`)
-    :rtype: float
-    """
-    return 320 * (spt_n60 + 15)
+#     :return:
+#         Elastic modulus of soil (:math:`kN/m^2`)
+#     :rtype: float
+#     """
+#     return 320 * (spt_n60 + 15)
 
 
-@round_(ndigits=1)
-def rankine_est_min_foundation_depth(
-    allowable_bearing_capacity: float,
-    soil_unit_weight: float,
-    soil_friction_angle: float,
-) -> float:
-    r"""Return minimum depth of foundation estimated using ``Rankine's``
-    correlation. (m)
+# @round_(ndigits=1)
+# def rankine_est_min_foundation_depth(
+#     allowable_bearing_capacity: float,
+#     soil_unit_weight: float,
+#     soil_friction_angle: float,
+# ) -> float:
+#     r"""Return minimum depth of foundation estimated using ``Rankine's``
+#     correlation. (m)
 
-    :param float allowable_bearing_capacity:
-        Allowable bearing capacity (:math:`kN/m^2`)
-    :param float soil_unit_weight:
-        Unit weight of soil (:math:`kN/m^3`)
-    :param float soil_friction_angle:
-        Internal angle of friction (degrees)
+#     :param float allowable_bearing_capacity:
+#         Allowable bearing capacity (:math:`kN/m^2`)
+#     :param float soil_unit_weight:
+#         Unit weight of soil (:math:`kN/m^3`)
+#     :param float soil_friction_angle:
+#         Internal angle of friction (degrees)
 
-    :return:
-        depth of foundation (m)
-    :rtype: float
-    """
-    exp_1 = allowable_bearing_capacity / soil_unit_weight
-    exp_2 = (1 - sin(soil_friction_angle)) / (1 + sin(soil_friction_angle))
+#     :return:
+#         depth of foundation (m)
+#     :rtype: float
+#     """
+#     exp_1 = allowable_bearing_capacity / soil_unit_weight
+#     exp_2 = (1 - sin(soil_friction_angle)) / (1 + sin(soil_friction_angle))
 
-    return exp_1 * (exp_2**2)
+#     return exp_1 * (exp_2**2)
--- tests/test_estimators.py
+++ tests/test_estimators.py
@@ -2,69 +2,69 @@ import pytest
 
 from geolysis import ERROR_TOLERANCE
 from geolysis.estimators import (
-    CompressionIndex,
-    SoilFrictionAngle,
+    CompressionIndexEst,
+    SoilFrictionAngleEst,
     SoilUnitWeight,
-    UndrainedShearStrength,
-    bowles_est_soil_elastic_modulus,
-    rankine_est_min_foundation_depth,
+    UndrainedShearStrengthEst,
 )
 
 
 def test_compression_index():
-    comp_idx = CompressionIndex(liquid_limit=35)
-    assert comp_idx.skempton_1994() == pytest.approx(0.175, ERROR_TOLERANCE)
-    assert comp_idx.terzaghi_et_al_1967() == pytest.approx(
-        0.225, ERROR_TOLERANCE
+    assert CompressionIndexEst.skempton_1994(liquid_limit=35) == pytest.approx(
+        0.175, ERROR_TOLERANCE
     )
+    assert CompressionIndexEst.terzaghi_et_al_1967(
+        liquid_limit=35
+    ) == pytest.approx(0.225, ERROR_TOLERANCE)
 
-    comp_idx = CompressionIndex(void_ratio=0.78)
-    assert comp_idx.hough_1957() == pytest.approx(0.148, ERROR_TOLERANCE)
+    assert CompressionIndexEst.hough_1957(void_ratio=0.78) == pytest.approx(
+        0.148, ERROR_TOLERANCE
+    )
 
 
 def test_soil_friction_angle():
-    sfa = SoilFrictionAngle(spt_n60=50)
-    assert sfa.wolff_1989() == pytest.approx(40.75, ERROR_TOLERANCE)
+    assert SoilFrictionAngleEst.wolff_1989(spt_n_60=50) == pytest.approx(
+        40.75, ERROR_TOLERANCE
+    )
 
-    sfa.spt_n60 = 40
-    assert sfa.wolff_1989() == pytest.approx(38.236, ERROR_TOLERANCE)
+    assert SoilFrictionAngleEst.wolff_1989(spt_n_60=40) == pytest.approx(
+        38.236, ERROR_TOLERANCE
+    )
 
-    sfa = SoilFrictionAngle(spt_n60=40, eop=103.8, atm_pressure=101.325)
-    assert sfa.kullhawy_mayne_1990() == pytest.approx(46.874, ERROR_TOLERANCE)
+    assert SoilFrictionAngleEst.kullhawy_mayne_1990(
+        spt_n_60=40, eop=103.8, atm_pressure=101.325
+    ) == pytest.approx(46.874, ERROR_TOLERANCE)
 
 
 def test_soil_unit_weight():
-    suw = SoilUnitWeight(spt_n60=13)
-    assert suw.moist == pytest.approx(17.3, ERROR_TOLERANCE)
-    assert suw.saturated == pytest.approx(18.75, ERROR_TOLERANCE)
-    assert suw.submerged == pytest.approx(8.93, ERROR_TOLERANCE)
+    suw = SoilUnitWeight(spt_n_60=13)
+    assert suw.est_moist_wgt == pytest.approx(17.3, ERROR_TOLERANCE)
+    assert suw.est_saturated_wgt == pytest.approx(18.75, ERROR_TOLERANCE)
+    assert suw.est_submerged_wgt == pytest.approx(8.93, ERROR_TOLERANCE)
 
 
 def test_undrained_shear_strength():
-    uss = UndrainedShearStrength(spt_n60=40)
-    assert uss.stroud_1974() == pytest.approx(140, ERROR_TOLERANCE)
+    assert UndrainedShearStrengthEst.stroud_1974(spt_n_60=40) == pytest.approx(
+        140, ERROR_TOLERANCE
+    )
 
-    uss.eop = 108.3
-    uss.plasticity_index = 12
-
-    assert uss.skempton_1957() == pytest.approx(16.722, ERROR_TOLERANCE)
-
-    uss.k = 7.0
+    assert UndrainedShearStrengthEst.skempton_1957(
+        eop=108.3, plasticity_index=12
+    ) == pytest.approx(16.722, ERROR_TOLERANCE)
 
     with pytest.raises(ValueError):
-        uss.stroud_1974()
-    # assert uss.stroud_1974()
+        UndrainedShearStrengthEst.stroud_1974(spt_n_60=30, k=7)
 
 
-def test_foundation_depth():
-    est_depth = rankine_est_min_foundation_depth(
-        allowable_bearing_capacity=350,
-        soil_unit_weight=18,
-        soil_friction_angle=35,
-    )
-    assert est_depth == pytest.approx(1.4, ERROR_TOLERANCE)
+# def test_foundation_depth():
+#     est_depth = rankine_est_min_foundation_depth(
+#         allowable_bearing_capacity=350,
+#         soil_unit_weight=18,
+#         soil_friction_angle=35,
+#     )
+#     assert est_depth == pytest.approx(1.4, ERROR_TOLERANCE)
 
 
-def test_soil_elastic_modulus():
-    est_elastic_modulus = bowles_est_soil_elastic_modulus(spt_n60=11)
-    assert est_elastic_modulus == pytest.approx(8320, ERROR_TOLERANCE)
+# def test_soil_elastic_modulus():
+#     est_elastic_modulus = bowles_est_soil_elastic_modulus(spt_n60=11)
+#     assert est_elastic_modulus == pytest.approx(8320, ERROR_TOLERANCE)
