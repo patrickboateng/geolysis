bcc57cf810598ecd9e6b0497e3a00fd04697583c
Patrick Boateng <boatengpato.pb@gmail.com>
2024-01-05 13:34:15 +0000

    minor change

--- geolysis/bearing_capacity/_base.py
+++ geolysis/bearing_capacity/_base.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 from geolysis.exceptions import AllowableSettlementError
+from geolysis.utils import PI, round_
 
 
 def _chk_settlement(actual_settlement: float, allowable_settlement: float):
@@ -14,46 +15,55 @@ def _chk_settlement(actual_settlement: float, allowable_settlement: float):
 class CircularFooting:
     """Circular Footing Size.
 
-    :param float width:
-        Diameter of foundation footing (m)
+    :param float width: Diameter of foundation footing (m)
     """
 
     width: float
+
+    @property
+    @round_(ndigits=2)
+    def area_of_footing(self):
+        return PI * self.width**2 / 4
 
 
 @dataclass
 class SquareFooting:
     """Square Footing Size.
 
-    :param float width:
-        Width of foundation footing (m)
+    :param float width: Width of foundation footing (m)
     """
 
     width: float
+
+    @property
+    @round_(ndigits=2)
+    def area_of_footing(self):
+        return self.width**2
 
 
 @dataclass
 class RectangularFooting:
     """Rectangular Footing Size.
 
-    :param float length:
-        Length of foundation footing (m)
-    :param float width:
-        Width of foundation footing (m)
+    :param float length: Length of foundation footing (m)
+    :param float width: Width of foundation footing (m)
     """
 
     length: float
     width: float
+
+    @property
+    @round_(ndigits=2)
+    def area_of_footing(self):
+        return self.length * self.width
 
 
 @dataclass
 class FoundationSize:
     """A simple class representing a foundation.
 
-    :param float depth:
-        Depth of foundation (m)
-    :param footing_size:
-        Represents the type of footing size i.e. :class:`SquareFooting`,
+    :param float depth: Depth of foundation (m)
+    :param footing_size:Represents the type of footing size i.e. :class:`SquareFooting`,
         :class:`RectangularFooting` or :class:`CircularFooting`
     :type footing_size: SquareFooting | RectangularFooting | CircularFooting
     """
