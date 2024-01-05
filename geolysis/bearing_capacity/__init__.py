72f7e84274ec24b783f9ab7f4b776dd16676185e
Patrick Boateng <boatengpato.pb@gmail.com>
2024-01-05 13:33:43 +0000

    removed unnecessary imports

--- geolysis/bearing_capacity/__init__.py
+++ geolysis/bearing_capacity/__init__.py
@@ -8,6 +8,3 @@ from ._base import (
     SquareFooting,
     _chk_settlement,
 )
-from .bowles import bowles_abc_chl_1997
-from .meyerhof import meyerhof_abc_chl_1956
-from .terzaghi import terzaghi_peck_abc_chl_1948
