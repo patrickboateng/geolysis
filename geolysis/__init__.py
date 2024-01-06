a22d2b04f2a607fcbd6b8c0e0cc40aa74f2c0ec4
Patrick Boateng <boatengpato.pb@gmail.com>
2024-01-06 23:34:16 +0000

    added useful imports

--- geolysis/__init__.py
+++ geolysis/__init__.py
@@ -1,4 +1,40 @@
-from . import estimators, exceptions, soil_classifier, utils
-from .constants import GeotechEng
+"""
+
+:mod:`geolysis`
+===============
+
+Open-source geotechnical analysis tool for Python.
+
+Modules
+-------
+
+Sub-Packages
+------------
+
+Exceptions
+----------
+
+"""
+from . import bearing_capacity
+from .constants import ERROR_TOLERANCE, EngineerTypeError, GeotechEng
+from .estimators import (
+    CompressionIndexEst,
+    SoilFrictionAngleEst,
+    SoilUnitWeightEst,
+    UndrainedShearStrengthEst,
+)
+from .soil_classifier import (
+    AASHTO,
+    AL,
+    PSD,
+    USCS,
+    AASHTOClassification,
+    AtterbergLimits,
+    ParticleSizeDistribution,
+    ParticleSizes,
+    UnifiedSoilClassification,
+)
+
+globals().update(GeotechEng.__members__)
 
 __version__ = "0.2.0"
