5e79e2ad9810f79fbf9daab97afaee86d961b335
Patrick Boateng <boatengpato.pb@gmail.com>
2024-01-31 23:05:12 +0000

    refactored imports

--- geolysis/__init__.py
+++ geolysis/__init__.py
@@ -1,36 +1,4 @@
-"""
-
-:mod:`geolysis`
-===============
-
-Open-source geotechnical analysis tool for Python.
-
-Modules
--------
-
-Sub-Packages
-------------
-
-Exceptions
-----------
-
-"""
-from . import bearing_capacity, estimators, soil_classifier, spt
-from .constants import (
-    CLAY,
-    ERROR_TOL,
-    GRAVEL,
-    HIGH_PLASTICITY,
-    LOW_PLASTICITY,
-    ORGANIC,
-    POORLY_GRADED,
-    SAND,
-    SILT,
-    WELL_GRADED,
-    EngineerTypeError,
-    GeotechEng,
-)
-
-globals().update(GeotechEng.__members__)
+from . import bearing_capacity, constants, estimators, soil_classifier, spt
+from .exceptions import EngineerTypeError
 
 __version__ = "0.2.0"
