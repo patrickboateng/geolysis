0cf4a787beb332a21bae4bb451db1c902e466d62
Patrick Boateng <boatengpato.pb@gmail.com>
2024-02-01 11:41:15 +0000

    refactored module

--- geolysis/constants.py
+++ geolysis/constants.py
@@ -1,37 +1,11 @@
-import enum
+from dataclasses import dataclass
 
 ERROR_TOL = 0.01
 
-GRAVEL = "G"
-SAND = "S"
-CLAY = "C"
-SILT = "M"
-WELL_GRADED = "W"
-POORLY_GRADED = "P"
-ORGANIC = "O"
-LOW_PLASTICITY = "L"
-HIGH_PLASTICITY = "H"
 
-
-class EngineerTypeError(TypeError):
-    pass
-
-
-class GeotechEng(enum.IntEnum):
-    """Represents a set of names of Geotechnical Engineers whose theories have
-    been utilised in developing ``geolysis``.
-    """
-
-    BAZARAA = 1
-    GIBBS = 2
-    HANSEN = 3
-    HOUGH = 4
-    KULLHAWY = 5
-    LIAO = 6
-    MEYERHOF = 7
-    SKEMPTON = 8
-    STROUD = 9
-    TERZAGHI = 10
-    VESIC = 11
-    WOLFF = 12
-    PECK = 13
+@dataclass(frozen=True)
+class UNITS:
+    kilo_pascal = "kPa"
+    kilo_newton_per_cubic_metre = "kN/m**3"
+    degree = "degree"
+    unitless = ""
