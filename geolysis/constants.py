94e4c78567a8eaffeda19234a7d73370eb758978
Patrick Boateng <boatengpato.pb@gmail.com>
2024-02-01 18:44:07 +0000

    changed name: degree to degrees

--- geolysis/constants.py
+++ geolysis/constants.py
@@ -3,9 +3,9 @@ from dataclasses import dataclass
 ERROR_TOL = 0.01
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, slots=True)
 class UNITS:
     kilo_pascal = "kPa"
     kilo_newton_per_cubic_metre = "kN/m**3"
-    degree = "degree"
+    degrees = "degrees"
     unitless = ""
