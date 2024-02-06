a19ef610931ee32d32e27d2418ccb093259b4085
Patrick Boateng <boatengpato.pb@gmail.com>
2024-02-06 00:14:51 +0000

    worked on docstrings

--- geolysis/constants.py
+++ geolysis/constants.py
@@ -5,6 +5,14 @@ ERROR_TOL = 0.01
 
 @dataclass(frozen=True, slots=True)
 class UNITS:
+    """SI units manager for values returned by various functions.
+
+    .. note::
+
+        These values are compatible with the `pint <https://pint.readthedocs.io/en/stable/index.html>`_
+        library.
+    """
+
     kilo_pascal = "kPa"
     kilo_newton_per_cubic_metre = "kN/m**3"
     degrees = "degrees"
