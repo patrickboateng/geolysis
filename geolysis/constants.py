452654c0d718c4b7d4a01e3ce8faa53eb72b0b77
Patrick Boateng <boatengpato.pb@gmail.com>
2024-02-11 18:34:02 +0000

    DOC: minor change

--- geolysis/constants.py
+++ geolysis/constants.py
@@ -9,8 +9,8 @@ class UNITS:
 
     .. note::
 
-        These values are compatible with the `pint <https://pint.readthedocs.io/en/stable/index.html>`_
-        library.
+        These units are compatible with the `pint <https://pint.readthedocs.io/en/stable/index.html>`_
+        library unit system.
     """
 
     kilo_pascal = "kPa"
