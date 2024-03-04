6f75393a67d4bef3aec9d4c19d57ff1d1097cbaf
Patrick Boateng <boatengpato.pb@gmail.com>
2024-03-04 10:17:58 +0000

    CONF: changed default mode to dark mode

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,7 +82,7 @@ html_css_files = [
     "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css",
     "custom.css",
 ]
-html_context = {"default_mode": "light"}
+html_context = {"default_mode": "dark"}
 
 html_theme_options = {
     "header_links_before_dropdown": 6,
