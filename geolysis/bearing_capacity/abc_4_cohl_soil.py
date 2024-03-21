856de0e7453c6c7c97e76d55f3cf881956d299e2
Patrick Boateng <boatengpato.pb@gmail.com>
2024-03-21 19:50:38 +0000

    renamed file for better description of what the file does

--- geolysis/bearing_capacity/abc_4_cohl_soil.py
+++ geolysis/bearing_capacity/abc_4_cohl_soil.py
@@ -2,7 +2,7 @@ from geolysis.constants import UNITS
 from geolysis.foundation import FoundationSize
 from geolysis.utils import FloatOrInt, round_
 
-__all__ = ["BowlesABC1997", "MeyerhofABC1956", "TerzaghiABC1948"]
+__all__ = ["BowlesABC", "MeyerhofABC", "TerzaghiABC"]
 
 
 class AllowableSettlementError(ValueError):
@@ -15,15 +15,14 @@ def _chk_settlement(
     tol_settlement: FloatOrInt, max_tol_settlement: FloatOrInt
 ):
     if tol_settlement > max_tol_settlement:
-        errmsg = f"Settlement: {tol_settlement} should be less than or equal \
+        err_msg = f"Settlement: {tol_settlement} should be less than or equal \
                 Allowable Settlement: {max_tol_settlement}"
-        raise AllowableSettlementError(errmsg)
+        raise AllowableSettlementError(err_msg)
 
 
-class BowlesABC1997:
+class BowlesABC:
     """
-    Allowable bearing capacity for cohesionless soils according to ``Bowles
-    (1997)``.
+    Allowable bearing capacity for cohesionless soils according to ``Bowles``.
 
     :param FloatOrInt avg_corrected_spt_val: Statistical average of corrected
                                              SPT N-value (55% energy with overburden
@@ -31,9 +30,24 @@ class BowlesABC1997:
                                              influence zone i.e. ``0.5B`` to ``2B``.
     :param FloatOrInt tol_settlement: Tolerable settlement. (mm)
     :param FoundationSize foundation_size: Size of foundation.
+
+    :Example:
+
+        >>> from geolysis.bearing_capacity.abc_4_cohl_soil import BowlesABC1997
+        >>> from geolysis.foundation import FoundationSize, FootingSize, SquareFooting
+        >>> footing_shape = SquareFooting(width=1.2)
+        >>> footing_size = FootingSize(thickness=0.3, footing_shape=footing_shape)
+        >>> foundation_size = FoundationSize(depth=1.5, footing_size=footing_size)
+        >>> bowles_abc = BowlesABC1997(avg_corrected_spt_val=11,
+        ...                            tol_settlement=20,
+        ...                            foundation_size=foundation_size)
+        >>> bowles_abc.abc_cohl_4_isolated_foundation()
+        220.72
     """
 
+    #: Maximum tolerable settlement. (mm)
     MAX_TOL_SETTLEMENT = 25.4
+
     unit = UNITS.kPa
 
     def __init__(
@@ -45,9 +59,9 @@ class BowlesABC1997:
         self.avg_corrected_spt_val = avg_corrected_spt_val
         self.tol_settlement = tol_settlement
 
-        #: Depth of footing. (m)
+        #: Depth of foundation. (m)
         self.f_depth = foundation_size.depth
-        #: Width of footing. (m)
+        #: Width of foundation footing. (m)
         self.f_width = foundation_size.width
 
         _chk_settlement(self.tol_settlement, self.MAX_TOL_SETTLEMENT)
@@ -56,34 +70,17 @@ class BowlesABC1997:
         return "Bowles (1997)"
 
     @property
-    def fd(self):
-        r"""
+    def fd(self) -> float:
+        """
         Return the depth factor.
-
-        .. math::
-
-            f_d = 1 + 0.33 \cdot \frac{D_f}{B} \le 1.33
         """
         return min(1 + 0.33 * self.f_depth / self.f_width, 1.33)
 
     @round_(ndigits=2)
-    def abc_cohl_4_isolated_foundation(self):
-        r"""
+    def abc_4_isolated_foundation_1997(self) -> float:
+        """
         Return allowable bearing capacity for isolated foundation on
         cohesionless soils.
-
-        for B :math:`\le` 1.2m:
-
-        .. math::
-
-            q_a(kPa) = 19.16(N_1)_{55} f_d\left(\dfrac{S}{25.4}\right)
-
-        for B :math:`\gt` 1.2m:
-
-        .. math::
-
-            q_a(kPa) = 11.8(N_1)_{55}\left(\dfrac{3.28B + 1}{3.28B} \right)^2 f_d
-            \left(\dfrac{S}{25.4}\right)
         """
 
         settlement_ratio = self.tol_settlement / self.MAX_TOL_SETTLEMENT
@@ -102,23 +99,18 @@ class BowlesABC1997:
         )
 
     @round_(ndigits=2)
-    def abc_cohl_4_raft_foundation(self):
-        r"""
+    def abc_4_raft_foundation_1997(self):
+        """
         Return allowable bearing capacity for raft foundation on cohesionless
         soils.
-
-        .. math::
-
-            q_a(kPa) = 11.98(N_1)_{55}f_d\left(\dfrac{S}{25.4}\right)
         """
         settlement_ratio = self.tol_settlement / self.MAX_TOL_SETTLEMENT
         return 11.98 * self.avg_corrected_spt_val * self.fd * settlement_ratio
 
 
-class MeyerhofABC1956:
+class MeyerhofABC:
     """
-    Allowable bearing capacity for cohesionless soils according to ``Meyerhof
-    (1956)``.
+    Allowable bearing capacity for cohesionless soils according to ``Meyerhof``.
 
     :param FloatOrInt avg_uncorrected_spt_val: Average uncorrected SPT N-value
                                                within the foundation influence
@@ -128,7 +120,9 @@ class MeyerhofABC1956:
     :param FoundationSize foundation_size: Size of foundation.
     """
 
+    #: Maximum tolerable settlement. (mm)
     MAX_TOL_SETTLEMENT = 25.4
+
     unit = UNITS.kPa
 
     def __init__(
@@ -152,32 +146,16 @@ class MeyerhofABC1956:
 
     @property
     def fd(self):
-        r"""
+        """
         Return the depth factor.
-
-        .. math::
-
-            f_d = 1 + 0.33 \cdot \frac{D_f}{B} \le 1.33
         """
         return min(1 + 0.33 * self.f_depth / self.f_width, 1.33)
 
     @round_(ndigits=2)
-    def abc_cohl_4_isolated_foundation(self):
-        r"""
+    def abc_4_isolated_foundation_1956(self):
+        """
         Return allowable bearing capacity for isolated foundation on
         cohesionless soils.
-
-        for B :math:`\le` 1.2m:
-
-        .. math::
-
-            q_a(kPa) = 12N f_d\left(\dfrac{S}{25.4}\right)
-
-        for B :math:`\gt` 1.2m:
-
-        .. math::
-
-            q_a(kPa) = 8N\left(\dfrac{3.28B + 1}{3.28B} \right)^2 f_d\left(\dfrac{S}{25.4}\right)
         """
         settlement_ratio = self.tol_settlement / self.MAX_TOL_SETTLEMENT
 
@@ -195,20 +173,16 @@ class MeyerhofABC1956:
         )
 
     @round_(ndigits=2)
-    def abc_cohl_4_raft_foundation(self):
-        r"""
+    def abc_4_raft_foundation_1956(self):
+        """
         Return allowable bearing capacity for raft foundation on cohesionless
         soils.
-
-        .. math::
-
-            q_a(kPa) = 8 N f_d\left(\dfrac{S}{25.4}\right)
         """
         settlement_ratio = self.tol_settlement / self.MAX_TOL_SETTLEMENT
         return 8 * self.avg_uncorrected_spt_val * self.fd * settlement_ratio
 
 
-class TerzaghiABC1948:
+class TerzaghiABC:
     """
     Allowable bearing capacity for cohesionless soils according to ``Terzaghi &
     Peck (1948)``.
@@ -222,7 +196,9 @@ class TerzaghiABC1948:
     :param FoundationSize foundation_size: Size of foundation.
     """
 
+    #: Maximum allowable settlement. (mm)
     MAX_TOL_SETTLEMENT = 25.4
+
     unit = UNITS.kPa
 
     def __init__(
@@ -236,9 +212,9 @@ class TerzaghiABC1948:
         self.tol_settlement = tol_settlement
         self.water_depth = water_depth
 
-        #: Depth of footing. (m)
+        #: Depth of foundation. (m)
         self.f_depth = foundation_size.depth
-        #: Width of footing. (m)
+        #: Width of foundation footing. (m)
         self.f_width = foundation_size.width
 
         _chk_settlement(self.tol_settlement, self.MAX_TOL_SETTLEMENT)
@@ -248,31 +224,15 @@ class TerzaghiABC1948:
 
     @property
     def fd(self):
-        r"""
+        """
         Return the depth factor.
-
-        .. math::
-
-            f_d = 1 + 0.25 \cdot \frac{D_f}{B} \le 1.25
         """
         return min(1 + 0.25 * self.f_depth / self.f_width, 1.25)
 
     @property
     def cw(self):
-        r"""
+        """
         Return the water correction factor.
-
-        for surface footing:
-
-        .. math::
-
-            c_w = 2 - \frac{D_w}{2B} \le 2
-
-        for fully submerged footing :math:`d_w \le D_f`
-
-        .. math::
-
-            c_w = 2 - \frac{D_f}{2B} \le 2
         """
         if self.water_depth <= self.f_depth:
             # for fully submerged footing
@@ -284,23 +244,10 @@ class TerzaghiABC1948:
         return min(_cw, 2)
 
     @round_(ndigits=2)
-    def abc_cohl_4_isolated_foundation(self):
-        r"""
+    def abc_4_isolated_foundation_1948(self) -> float:
+        """
         Return allowable bearing capacity for isolated foundation on
         cohesionless soils.
-
-        for B :math:`\le` 1.2m:
-
-        .. math::
-
-            q_a(kPa) = 12N \dfrac{1}{c_w f_d}\left(\dfrac{S}{25.4}\right)
-
-        for B :math:`\gt` 1.2m:
-
-        .. math::
-
-            q_a(kPa) = 8N\left(\dfrac{3.28B + 1}{3.28B} \right)^2\dfrac{1}{c_w f_d}
-            \left(\dfrac{S}{25.4}\right)
         """
         settlement_ratio = self.tol_settlement / self.MAX_TOL_SETTLEMENT
 
@@ -321,14 +268,10 @@ class TerzaghiABC1948:
         )
 
     @round_(ndigits=2)
-    def abc_cohl_4_raft_foundation(self):
-        r"""
+    def abc_4_raft_foundation_1948(self) -> float:
+        """
         Return allowable bearing capacity for raft foundation on cohesionless
         soils.
-
-        .. math::
-
-            q_a(kPa) = 8N\dfrac{1}{c_w f_d}\left(\dfrac{S}{25.4}\right)
         """
         settlement_ratio = self.tol_settlement / self.MAX_TOL_SETTLEMENT
 
