872efb485940f4c6d2154e9b04e32aff59979681
Patrick Boateng <boatengpato.pb@gmail.com>
2024-04-29 14:13:34 +0000

    added tests for all objects in spt.py

---
+++ tests/test_spt.py
@@ -0,0 +1,51 @@
+import unittest
+
+from geolysis.spt import (
+    BazaraaPeckOPC,
+    DilatancyCorrection,
+    GibbsHoltzOPC,
+    LiaoWhitmanOPC,
+    OPCError,
+    PeckOPC,
+)
+
+
+class TestGibbsHoltzOPC(unittest.TestCase):
+
+    def testErrors(self):
+        with self.assertRaises(OPCError):
+            GibbsHoltzOPC(std_spt_number=15.4, eop=0)
+
+        with self.assertRaises(OPCError):
+            GibbsHoltzOPC(std_spt_number=11.0, eop=300)
+
+
+class TestBazaraaPeckOPC(unittest.TestCase):
+
+    def testCorrection(self):
+        cor = BazaraaPeckOPC(std_spt_number=11.4, eop=71.8)
+        self.assertEqual(cor.correction, 1.0)
+
+        cor.eop = 54.8
+        self.assertAlmostEqual(cor.correction, 1.22)
+
+
+class TestPeckOPC(unittest.TestCase):
+
+    def testErrors(self):
+        with self.assertRaises(OPCError):
+            PeckOPC(std_spt_number=12.8, eop=15.8)
+
+
+class TestLiaoWhitmanOPC(unittest.TestCase):
+
+    def testErrors(self):
+        with self.assertRaises(OPCError):
+            LiaoWhitmanOPC(std_spt_number=16.7, eop=0.0)
+
+
+class TestDilatancyCorrection(unittest.TestCase):
+
+    def testCorrectedSPTNumber(self):
+        cor = DilatancyCorrection(spt_number=12.6)
+        self.assertAlmostEqual(cor.corrected_spt_number, 12.6)
