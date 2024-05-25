6ad136437dff8884e2f19ed3ce317945999d6528
Patrick Boateng <boatengpato.pb@gmail.com>
2024-05-25 22:24:43 +0000

    modified code documentation

--- geolysis/core/constants.py
+++ geolysis/core/constants.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from typing import Any
 
 __all__ = ["DECIMAL_PLACES", "ERROR_TOL", "UNIT"]
 
@@ -17,36 +16,32 @@ class UNIT:
     """Physical units manager for values returned by various functions
     (or methods) that returns a float.
 
-    Attributes
-    ----------
-    m : str
-        meter
-    mm : str
-        millimeter
-    kg : str
-        kilogram
-    deg : str
-        degree
-    m2 : str
-        square meter
-    m3 : str
-        cubic meter
-    kPa : str
-        kilo Pascal
-    kN_m3 : str
-        kilo Newton per cubic meter
-
     Notes
     -----
     These units are compatible with the `pint <https://pint.readthedocs.io/en/stable/index.html>`_
     library unit system.
     """
 
+    #: meter
     m = "meter"
+
+    #: millimeter
     mm = "millimeter"
+
+    #: kilogram
     kg = "kilogram"
+
+    #: degree
     deg = "degrees"
+
+    #: square meter
     m2 = "m**2"
+
+    #: cubic meter
     m3 = "m**3"
+
+    #: kilo Pascal
     kPa = "kPa"
+
+    #: kilo Newton per cubic meter
     kN_m3 = "kN/m**3"
