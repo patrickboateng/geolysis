cf1d9a7be4383bf1cc721fc9b4818d9373b8f609
Patrick Boateng <boatengpato.pb@gmail.com>
2024-07-03 11:28:17 +0000

    minor change

--- geolysis/core/bearing_capacity/__init__.py
+++ geolysis/core/bearing_capacity/__init__.py
@@ -1,16 +1,16 @@
 from .abc_4_cohl_soils import (
-    BowlesABC4MatFoundation,  # noqa: F401
-    BowlesABC4PadFoundation,  # noqa: F401
-    MeyerhofABC4MatFoundation,  # noqa: F401
-    MeyerhofABC4PadFoundation,  # noqa: F401
-    TerzaghiABC4MatFoundation,  # noqa: F401
-    TerzaghiABC4PadFoundation,  # noqa: F401
+    BowlesABC4MatFoundation,
+    BowlesABC4PadFoundation,
+    MeyerhofABC4MatFoundation,
+    MeyerhofABC4PadFoundation,
+    TerzaghiABC4MatFoundation,
+    TerzaghiABC4PadFoundation,
 )
 from .ubc import (
-    HansenUBC,  # noqa: F401
-    TerzaghiUBC4CircFooting,  # noqa: F401
-    TerzaghiUBC4RectFooting,  # noqa: F401
-    TerzaghiUBC4SquareFooting,  # noqa: F401
-    TerzaghiUBC4StripFooting,  # noqa: F401
-    VesicUBC,  # noqa: F401
+    HansenUBC,
+    TerzaghiUBC4CircFooting,
+    TerzaghiUBC4RectFooting,
+    TerzaghiUBC4SquareFooting,
+    TerzaghiUBC4StripFooting,
+    VesicUBC,
 )
