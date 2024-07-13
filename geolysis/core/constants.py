3169348ef5e020a6d58f6279aeaadad21cc46917
Patrick Boateng <boatengpato.pb@gmail.com>
2024-07-13 13:12:21 +0000

    designed a Conf object for setting options for the geolysis application

--- geolysis/core/constants.py
+++ geolysis/core/constants.py
@@ -1,19 +1,119 @@
 from collections import UserDict
 from dataclasses import dataclass
 from numbers import Number
+from typing import Any, Callable, NamedTuple
 
-__all__ = ["DECIMAL_PLACES", "ERROR_TOL", "UNIT"]
-
-#: The number of decimal places to round mathematical
-#: values returned from functions (or methods) to.
-DECIMAL_PLACES: int = 4
-
-#: Allowable error tolerance for mathematical values
-#: returned from functions (or methods).
-ERROR_TOL: float = 0.01
+__all__ = ["UNIT"]
 
 
-class SoilProperties(UserDict):
+class RegisteredOption(NamedTuple):
+    key: str
+    defval: Any
+    doc: str
+    validator: Callable | None
+
+
+class _InstanceOfValidator:
+    def __init__(self, type) -> None:
+        self.type = type
+
+    def __call__(self, val):
+        if not isinstance(val, self.type):
+            err_msg = (
+                f"Must be {self.type!r}, got {val!r} that is {type(val)!r}"
+            )
+            raise TypeError(err_msg)
+
+
+class validators:
+    @staticmethod
+    def instance_of(type):
+        """A validator that raises a `TypeError` if the initializer
+        is called with a wrong type for this particular option.
+
+        Parameters
+        ----------
+        type : Any | tuple[Any]
+            The type to check for.
+
+        Raises
+        ------
+        TypeError
+        """
+        return _InstanceOfValidator(type)
+
+
+class Config:
+    _global_config = {}
+    _registered_options: dict[str, RegisteredOption] = {}
+    _reserved_keys: list[str] = []
+
+    @classmethod
+    def _get_registered_option(cls, opt: str) -> Any:
+        return cls._registered_options.get(opt)
+
+    @classmethod
+    def get_option(cls, opt: str) -> Any:
+        return cls._global_config[opt]
+
+    @classmethod
+    def set_option(cls, opt: str, val: Any) -> None:
+        _opt = cls._get_registered_option(opt)
+
+        if _opt and _opt.validator:
+            _opt.validator(val)
+
+        cls._global_config[opt] = val
+
+    @classmethod
+    def reset_option(cls, opt: str):
+        val = cls._registered_options[opt].defval
+        cls.set_option(opt, val)
+
+    @classmethod
+    def register_option(cls, opt: str, defval: Any, doc="", validator=None):
+        import keyword
+
+        opt = opt.casefold()
+
+        if opt in cls._registered_options:
+            raise Exception
+
+        if opt in cls._reserved_keys:
+            raise Exception
+
+        if keyword.iskeyword(opt):
+            raise ValueError
+
+        if validator:
+            validator(defval)
+
+        cls._global_config[opt] = defval
+
+        cls._registered_options[opt] = RegisteredOption(
+            key=opt,
+            defval=defval,
+            doc=doc,
+            validator=validator,
+        )
+
+
+Config.register_option("dp", 4, validator=validators.instance_of(int))
+# Config.register_option(
+#     "error_tol", 0.01, validator=validators.instance_of(Number)
+# )
+
+
+# The number of decimal places to round mathematical
+# values returned from functions (or methods) to.
+# DECIMAL_PLACES: int = 4
+
+# Allowable error tolerance for mathematical values
+# returned from functions (or methods).
+# ERROR_TOL: float = 0.01
+
+
+class SoilData(UserDict):
     def __getattr__(self, attr) -> Number:
         return self.data[attr]
 
