111121ba67af2d471d33e67608ed9a59b225745c
Patrick Boateng <boatengpato.pb@gmail.com>
2024-09-29 13:05:18 -0500

    changed some properties to methods

--- tests/test_spt.py
+++ tests/test_spt.py
@@ -5,39 +5,38 @@ from geolysis.core.spt import (
     DilatancyCorrection,
     GibbsHoltzOPC,
     LiaoWhitmanOPC,
-    OPCError,
     PeckOPC,
 )
 
 
 class TestGibbsHoltzOPC(unittest.TestCase):
     def testErrors(self):
-        with self.assertRaises(OPCError):
-            GibbsHoltzOPC(std_spt_number=15.4, eop=0)
+        with self.assertRaises(ValueError):
+            GibbsHoltzOPC(std_spt_number=15.4, eop=0.0)
 
-        with self.assertRaises(OPCError):
-            GibbsHoltzOPC(std_spt_number=11.0, eop=300)
+        with self.assertRaises(ValueError):
+            GibbsHoltzOPC(std_spt_number=11.0, eop=300.0)
 
 
 class TestBazaraaPeckOPC(unittest.TestCase):
     def testCorrection(self):
         cor = BazaraaPeckOPC(std_spt_number=11.4, eop=54.8)
-        self.assertAlmostEqual(cor.corrected_spt_number, 14.0)
+        self.assertAlmostEqual(cor.corrected_spt_number(), 13.9)
 
 
 class TestPeckOPC(unittest.TestCase):
     def testErrors(self):
-        with self.assertRaises(OPCError):
+        with self.assertRaises(ValueError):
             PeckOPC(std_spt_number=12.8, eop=15.8)
 
 
 class TestLiaoWhitmanOPC(unittest.TestCase):
     def testErrors(self):
-        with self.assertRaises(OPCError):
+        with self.assertRaises(ValueError):
             LiaoWhitmanOPC(std_spt_number=16.7, eop=0.0)
 
 
 class TestDilatancyCorrection(unittest.TestCase):
     def testCorrectedSPTNumber(self):
-        cor = DilatancyCorrection(spt_number=12.6)
-        self.assertAlmostEqual(cor.corrected_spt_number, 12.6)
+        cor = DilatancyCorrection(std_spt_number=12.6)
+        self.assertAlmostEqual(cor.corrected_spt_number(), 12.6)
