3a1ecac4eca7e12907e34f21c7bc09ae2bcdb049
Patrick Boateng <boatengpato.pb@gmail.com>
2024-10-04 15:16:51 -0500

    removed outdated test cases

--- tests/test_foundation.py
+++ tests/test_foundation.py
@@ -2,7 +2,6 @@ import unittest
 
 from geolysis.core.foundation import (
     CircularFooting,
-    FoundationSize,
     SquareFooting,
 )
 
@@ -24,18 +23,3 @@ class TestSquareFooting(unittest.TestCase):
         sqr_footing = SquareFooting(1.2)
         sqr_footing.length = 1.4
         self.assertAlmostEqual(sqr_footing.width, 1.4)
-
-
-class TestFoundationSize(unittest.TestCase):
-    def testAttributes(self):
-        fs = FoundationSize(depth=1.5, footing_size=SquareFooting(width=1.2))
-
-        fs.width = 1.4
-        self.assertAlmostEqual(fs.width, 1.4)
-
-        fs.length = 1.5
-        self.assertAlmostEqual(fs.length, 1.5)
-
-        footing_type = CircularFooting(diameter=1.5)
-        fs.footing_size = footing_type
-        self.assertEqual(fs.footing_size, CircularFooting(diameter=1.5))
