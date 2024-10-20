e587282683aa5c361d03df4ed737882481e1688b
Patrick Boateng <boatengpato.pb@gmail.com>
2024-10-19 23:38:26 -0500

    added default decimal places for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,5 +6,7 @@ from geolysis.core import UnitSystem, reset_option, set_option
 @pytest.fixture(scope="session")
 def configure():
     set_option("unit_system", UnitSystem.SI)
+    set_option("dp", 4)
     yield
     reset_option("unit_system")
+    reset_option("dp")
