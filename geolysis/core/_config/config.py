c435a105b5c3c5680e3befdc7919bbbc833c90c5
Patrick Boateng <boatengpato.pb@gmail.com>
2024-10-19 23:34:58 -0500

    .\geolysis\core\_config\config.py
    moved file to config folder

--- geolysis/core/_config/config.py
+++ geolysis/core/_config/config.py
@@ -3,6 +3,8 @@ from typing import Any, Callable, NamedTuple
 
 from pint import UnitRegistry
 
+from geolysis.core._config import validators
+
 UNIT_REGISTRY = UnitRegistry()
 Q_ = UNIT_REGISTRY.Quantity
 
@@ -41,36 +43,6 @@ _registered_options: dict[str, RegisteredOption] = {}
 _reserved_keys: list[str] = []
 
 
-class _InstanceOfValidator:
-    def __init__(self, type) -> None:
-        self.type = type
-
-    def __call__(self, val):
-        if not isinstance(val, self.type):
-            err_msg = (
-                f"Must be {self.type!r}, got {val!r} that is {type(val)!r}"
-            )
-            raise TypeError(err_msg)
-
-
-class validators:
-    @staticmethod
-    def instance_of(type):
-        """A validator that raises a `TypeError` if the initializer
-        is called with a wrong type for this particular option.
-
-        Parameters
-        ----------
-        type : Any | tuple[Any]
-            The type to check for.
-
-        Raises
-        ------
-        TypeError
-        """
-        return _InstanceOfValidator(type)
-
-
 def _get_registered_option(opt: str) -> Any:
     return _registered_options.get(opt)
 
@@ -96,6 +68,9 @@ def reset_option(opt: str):
 def register_option(opt: str, defval: Any, doc="", validator=None):
     import keyword
 
+    if validator:
+        validator(defval)
+
     opt = opt.casefold()
 
     if opt in _registered_options:
@@ -107,9 +82,6 @@ def register_option(opt: str, defval: Any, doc="", validator=None):
     if keyword.iskeyword(opt):
         raise ValueError
 
-    if validator:
-        validator(defval)
-
     _global_config[opt] = defval
 
     _registered_options[opt] = RegisteredOption(
@@ -120,5 +92,11 @@ def register_option(opt: str, defval: Any, doc="", validator=None):
     )
 
 
-register_option("dp", 4, validator=validators.instance_of(int))
-register_option("unit_system", UnitSystem.SI)
+@enum.global_enum
+class OPTIONS(enum.StrEnum):
+    DP = enum.auto()
+    UNIT_SYSTEM = enum.auto()
+
+
+register_option(OPTIONS.DP, defval=4, validator=validators.instance_of(int))
+register_option(OPTIONS.UNIT_SYSTEM, defval=UnitSystem.SI)
