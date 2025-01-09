57303cb4eaf49c05f8ea0ae9ca752f495f4be4f1
Patrick Boateng <boatengpato.pb@gmail.com>
2025-01-09 12:25:30 -0600

    added module imports

--- geolysis/bearing_capacity/__init__.py
+++ geolysis/bearing_capacity/__init__.py
@@ -1,7 +1,8 @@
 from collections import UserDict
 from typing import TypedDict
 
-__all__ = ["SoilProperties", "Soil"]
+__all__ = ["SoilProperties", "Soil", "abc_4_cohl_soils", "hansen_ubc",
+           "terzaghi_ubc", "vesic_ubc"]
 
 
 class SoilProperties(TypedDict):
@@ -18,3 +19,7 @@ class Soil(UserDict):
         else:
             raise AttributeError(
                 f"'{self.__class__.__name__}' object has no attribute '{name}'")
+
+
+from .abc_4_soils import abc_4_cohl_soils
+from .ubc_4_soils import hansen_ubc, vesic_ubc, terzaghi_ubc
