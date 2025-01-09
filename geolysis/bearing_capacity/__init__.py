b2c765cde73cc7c93f6996247a776319d544ee50
Patrick Boateng <boatengpato.pb@gmail.com>
2025-01-09 00:08:18 -0600

    removed all imports and added classes shared among modules

--- geolysis/bearing_capacity/__init__.py
+++ geolysis/bearing_capacity/__init__.py
@@ -1,16 +1,20 @@
-from .abc_4_soils.abc_4_cohl_soils import (
-    BowlesABC4MatFoundation,
-    BowlesABC4PadFoundation,
-    MeyerhofABC4MatFoundation,
-    MeyerhofABC4PadFoundation,
-    TerzaghiABC4MatFoundation,
-    TerzaghiABC4PadFoundation,
-)
-from .ubc_4_soils.hansen_ubc import HansenUltimateBearingCapacity
-from .ubc_4_soils.terzaghi_ubc import (
-    TerzaghiUBC4CircFooting,
-    TerzaghiUBC4RectFooting,
-    TerzaghiUBC4SquareFooting,
-    TerzaghiUBC4StripFooting,
-)
-from .ubc_4_soils.vesic_ubc import VesicUltimateBearingCapacity
+from collections import UserDict
+from typing import TypedDict
+
+__all__ = ["SoilProperties", "Soil"]
+
+
+class SoilProperties(TypedDict):
+    friction_angle: float
+    cohesion: float
+    moist_unit_wgt: float
+
+
+class Soil(UserDict):
+
+    def __getattr__(self, name: str):
+        if name in self.data:
+            return self.data[name]
+        else:
+            raise AttributeError(
+                f"'{self.__class__.__name__}' object has no attribute '{name}'")
