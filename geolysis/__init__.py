3a795d9a875daf70c7a603f677fdac753ff6b14f
Patrick Boateng <boatengpato.pb@gmail.com>
2025-01-09 12:22:39 -0600

    organized imports

--- geolysis/__init__.py
+++ geolysis/__init__.py
@@ -1,9 +1,3 @@
-from . import (
-    foundation,
-    soil_classifier,
-    spt,
-    utils,
-)
-from .bearing_capacity.abc_4_soils import abc_4_cohl_soils
+from . import foundation, soil_classifier, spt, utils
 
 __version__ = "0.4.2"
