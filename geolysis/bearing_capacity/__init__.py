bd386fb7286b0d65340eb304fb296c8b60005a3a
Patrick Boateng <boatengpato.pb@gmail.com>
2025-01-11 14:38:58 -0600

    custom dict inherit from builtin dict instead of UserDict

--- geolysis/bearing_capacity/__init__.py
+++ geolysis/bearing_capacity/__init__.py
@@ -5,20 +5,20 @@ __all__ = ["SoilProperties", "Soil", "abc_4_cohl_soils", "hansen_ubc",
            "terzaghi_ubc", "vesic_ubc"]
 
 
-class SoilProperties(TypedDict):
+class SoilProperties(TypedDict, total=False):
     friction_angle: float
     cohesion: float
     moist_unit_wgt: float
 
 
-class Soil(UserDict):
+class Soil(dict):
 
     def __getattr__(self, name: str):
-        if name in self.data:
-            return self.data[name]
+        if name in self:
+            return super().__getitem__(name)
         else:
-            raise AttributeError(
-                f"'{self.__class__.__name__}' object has no attribute '{name}'")
+            msg = f"'{self.__class__.__name__}' object has no attribute '{name}'"
+            raise AttributeError(msg)
 
 
 from .abc_4_soils import abc_4_cohl_soils
