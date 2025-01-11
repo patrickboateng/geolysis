890e3f8b8dc609bcb6413d15844233bb1d08d5eb
Patrick Boateng <boatengpato.pb@gmail.com>
2025-01-11 16:29:32 -0600

    minor change

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,47 +1,38 @@
 import pytest
 
-from geolysis.utils import (
-    PI,
-    cos,
-    cot,
-    deg2rad,
-    log10,
-    rad2deg,
-    sin,
-    sqrt,
-    tan,
-)
+from geolysis.utils import (pi, cos, cot, deg2rad, log10, rad2deg, sin, sqrt,
+                            tan)
 
 ERROR_TOL = 0.01
 
 
 def test_deg2rad():
-    assert deg2rad(180) == pytest.approx(PI, ERROR_TOL)
+    assert deg2rad(180.0) == pytest.approx(pi, ERROR_TOL)
 
 
 def test_rad2deg():
-    assert rad2deg(PI) == pytest.approx(180, ERROR_TOL)
+    assert rad2deg(pi) == pytest.approx(180.0, ERROR_TOL)
 
 
 def test_tan():
-    assert tan(45) == pytest.approx(1, ERROR_TOL)
+    assert tan(45.0) == pytest.approx(1.0, ERROR_TOL)
 
 
 def test_cot():
-    assert cot(60) == pytest.approx(0.577, ERROR_TOL)
+    assert cot(60.0) == pytest.approx(0.577, ERROR_TOL)
 
 
 def test_sin():
-    assert sin(45) == pytest.approx(0.707, ERROR_TOL)
+    assert sin(45.0) == pytest.approx(0.707, ERROR_TOL)
 
 
 def test_cos():
-    assert cos(45) == pytest.approx(0.707, ERROR_TOL)
+    assert cos(45.0) == pytest.approx(0.707, ERROR_TOL)
 
 
 def test_log():
-    assert log10(10) == pytest.approx(1, ERROR_TOL)
+    assert log10(10.0) == pytest.approx(1.0, ERROR_TOL)
 
 
 def test_sqrt():
-    assert sqrt(25) == pytest.approx(5, ERROR_TOL)
+    assert sqrt(25.0) == pytest.approx(5.0, ERROR_TOL)
