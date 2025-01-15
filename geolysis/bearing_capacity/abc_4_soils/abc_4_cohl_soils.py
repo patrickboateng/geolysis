29b943e614ca0e8d3e6a38ea000c8184b8cb3e4f
Patrick Boateng <boatengpato.pb@gmail.com>
2025-01-14 20:58:14 -0600

    improved object docstrings

--- geolysis/bearing_capacity/abc_4_soils/abc_4_cohl_soils.py
+++ geolysis/bearing_capacity/abc_4_soils/abc_4_cohl_soils.py
@@ -13,7 +13,7 @@ class SettlementError(ValueError):
 
 
 class AllowableBearingCapacity(ABC):
-    #: Maximum tolerable foundation settlement.
+    #: Maximum tolerable foundation settlement (mm).
     MAX_TOL_SETTLEMENT = 25.4
 
     def __init__(self, corrected_spt_number: float, tol_settlement: float,
@@ -32,7 +32,7 @@ class AllowableBearingCapacity(ABC):
         self._tol_settlement = tol_settlement
 
     def _sr(self) -> float:
-        return self._tol_settlement / self.MAX_TOL_SETTLEMENT
+        return self.tol_settlement / self.MAX_TOL_SETTLEMENT
 
     def _fd(self) -> float:
         f_d = self.foundation_size.depth
@@ -42,7 +42,7 @@ class AllowableBearingCapacity(ABC):
     @staticmethod
     def _chk_settlement(tol_settlement: float, max_tol_settlement: float):
         if tol_settlement > max_tol_settlement:
-            err_msg = "tol_settlement should not be greater than 25.4."
+            err_msg = "tol_settlement should not be greater than 25.4mm"
             raise SettlementError(err_msg)
 
     @abstractmethod
@@ -50,172 +50,177 @@ class AllowableBearingCapacity(ABC):
 
 
 class BowlesABC4PadFoundation(AllowableBearingCapacity):
-    r"""Allowable bearing capacity for mat foundation on cohesionless
-    soils according to ``Bowles (1997)``.
-
-    Notes
-    -----
-    Allowable bearing capacity for ``isolated/pad/spread`` foundations:
-
-    .. math::
-
-        q_a(kPa) &= 19.16(N_1)_{55} f_d\left(\dfrac{S}{25.4}\right),
-                    \ B \ \le \ 1.2m
-
-        q_a(kPa) &= 11.98(N_1)_{55}\left(\dfrac{3.28B + 1}{3.28B} \right)^2
-                    f_d \left(\dfrac{S}{25.4}\right), \ B \ \gt 1.2m
-
-    Depth factor:
-
-    .. math:: f_d = 1 + 0.33 \cdot \frac{D_f}{B} \le 1.33
+    """Allowable bearing capacity for pad foundation on cohesionless soils
+    according to ``Bowles (1997)``.
     """
 
     def __init__(self, corrected_spt_number: float, tol_settlement: float,
                  foundation_size: FoundationSize) -> None:
         """
-        :param float corrected_spt_number: Statistical average of corrected SPT
-            N-value (55% energy with overburden pressure correction) within the
-            foundation influence zone i.e ``0.5B`` to ``2B``.
-        :param float tol_settlement: Tolerable settlement of foundation.
-        :param FoundationSize foundation_size: Size of foundation.
+        :param corrected_spt_number: Statistical average of corrected SPT
+                                     N-value (55% energy with overburden 
+                                     pressure correction) within the foundation 
+                                     influence zone i.e ``0.5B`` to ``2B``.
+        :type corrected_spt_number: float
+
+        :param tol_settlement: Tolerable settlement of foundation (mm).
+        :type tol_settlement: float
+
+        :param foundation_size: Size of foundation.
+        :type foundation_size: FoundationSize
         """
         super().__init__(corrected_spt_number, tol_settlement, foundation_size)
 
     @round_
     def bearing_capacity(self):
+        r""" Calculate the allowable bearing capacity of the pad foundation.
+
+        .. note:: 
+
+            Allowable bearing capacity:
+
+            .. math::
+
+                q_a(kPa) &= 19.16(N_1)_{55} f_d\left(\dfrac{S}{25.4}\right),
+                            \ B \ \le \ 1.2m
+
+                q_a(kPa) &= 11.98(N_1)_{55}\left(\dfrac{3.28B + 1}{3.28B} \right)^2
+                            f_d \left(\dfrac{S}{25.4}\right), \ B \ \gt 1.2m
+
+            Depth factor:
+
+            .. math:: f_d = 1 + 0.33 \cdot \frac{D_f}{B} \le 1.33
+        """
         n_corr = self.corrected_spt_number
         f_w = self.foundation_size.width
 
         if f_w <= 1.2:
             return 19.16 * n_corr * self._fd() * self._sr()
 
-        return (11.98 * n_corr * ((3.28 * f_w + 1) / (
-                3.28 * f_w)) ** 2 * self._fd() * self._sr())
+        return (11.98 * n_corr * ((3.28 * f_w + 1) / 
+               (3.28 * f_w)) ** 2 * self._fd() * self._sr())
 
 
 class BowlesABC4MatFoundation(BowlesABC4PadFoundation):
-    r"""Allowable bearing capacity for mat foundation on cohesionless
-    soils according to ``Bowles (1997)``.
-
-    Notes
-    -----
-    Allowable bearing capacity for ``raft/mat`` foundations:
-
-    .. math:: q_a(kPa) = 11.98(N_1)_{55}f_d\left(\dfrac{S}{25.4}\right)
-
-    Depth factor:
-
-    .. math:: f_d = 1 + 0.33 \cdot \frac{D_f}{B} \le 1.33
+    """Allowable bearing capacity for mat foundation on cohesionless soils
+    according to ``Bowles (1997)``.
     """
 
     @round_
     def bearing_capacity(self) -> float:
+        r""" Calculate the allowable bearing capacity of the mat foundation.
+
+
+        .. note::
+
+            Allowable bearing capacity:
+
+            .. math:: q_a(kPa) = 11.98(N_1)_{55}f_d\left(\dfrac{S}{25.4}\right)
+
+            Depth factor:
+
+            .. math:: f_d = 1 + 0.33 \cdot \frac{D_f}{B} \le 1.33
+        """
         n_corr = self.corrected_spt_number
         return 11.98 * n_corr * self._fd() * self._sr()
 
 
 class MeyerhofABC4PadFoundation(AllowableBearingCapacity):
-    r"""Allowable bearing capacity for pad foundation on cohesionless
-    soils according to ``Meyerhof (1956)``.
-
-    Notes
-    -----
-    Allowable bearing capacity for ``isolated/pad/spread`` foundations:
-
-    .. math::
-
-        q_a(kPa) &= 12N f_d\left(\dfrac{S}{25.4}\right), \ B \ \le 1.2m
-
-        q_a(kPa) &= 8N\left(\dfrac{3.28B + 1}{3.28B} \right)^2 f_d\left(
-                     \dfrac{S}{25.4}\right), \ B \ \gt 1.2m
-
-    Depth factor:
-
-    .. math:: f_d = 1 + 0.33 \cdot \frac{D_f}{B} \le 1.33
+    """Allowable bearing capacity for pad foundation on cohesionless soils
+    according to ``Meyerhof (1956)``.
     """
 
     def __init__(self, corrected_spt_number: float, tol_settlement: float,
                  foundation_size: FoundationSize):
         """
-        :param float corrected_spt_number: Average uncorrected SPT N-value (60%
-        energy with dilatancy (water) correction if applicable) within the
-        foundation influence zone i.e :math:`D_f` to :math:`D_f + 2B`.
-        :param float tol_settlement: Tolerable settlement of foundation.
-        :param FoundationSize foundation_size: Size of foundation.
+        :param corrected_spt_number: Average uncorrected SPT N-value (60% energy 
+                                     with dilatancy (water) correction if 
+                                     applicable) within the foundation influence 
+                                     zone i.e :math:`D_f` to :math:`D_f + 2B`.
+        :type corrected_spt_number: float
+
+        :param tol_settlement: Tolerable settlement of foundation (mm).
+        :type tol_settlement: float
+
+        :param foundation_size: Size of foundation.
+        :type foundation_size: FoundationSize
         """
         super().__init__(corrected_spt_number, tol_settlement, foundation_size)
 
     @round_
     def bearing_capacity(self):
+        r"""Calculates the allowable bearing capacity of the pad foundation.
+
+        .. note::
+
+            Allowable bearing capacity:
+
+            .. math::
+
+                q_a(kPa) &= 12N f_d\left(\dfrac{S}{25.4}\right), \ B \ \le 1.2m
+
+                q_a(kPa) &= 8N\left(\dfrac{3.28B + 1}{3.28B} \right)^2 f_d\left(
+                        \dfrac{S}{25.4}\right), \ B \ \gt 1.2m
+
+            Depth factor:
+
+            .. math:: f_d = 1 + 0.33 \cdot \frac{D_f}{B} \le 1.33 
+        """
         n_corr = self.corrected_spt_number
         f_w = self.foundation_size.width
 
         if f_w <= 1.2:
             return 12 * n_corr * self._fd() * self._sr()
 
-        return (8 * n_corr * ((3.28 * f_w + 1) / (
-                3.28 * f_w)) ** 2 * self._fd() * self._sr())
+        return (8 * n_corr * ((3.28 * f_w + 1) / 
+               (3.28 * f_w)) ** 2 * self._fd() * self._sr())
 
 
 class MeyerhofABC4MatFoundation(MeyerhofABC4PadFoundation):
-    r"""Allowable bearing capacity for mat foundation on cohesionless
+    """Allowable bearing capacity for mat foundation on cohesionless
     soils according to ``Meyerhof (1956)``.
-
-    Notes
-    -----
-    Allowable bearing capacity for ``raft/mat`` foundations:
-
-    .. math:: q_a(kPa) = 8 N f_d\left(\dfrac{S}{25.4}\right)
-
-    Depth factor:
-
-    .. math:: f_d = 1 + 0.33 \cdot \frac{D_f}{B} \le 1.33
     """
 
     @round_
     def bearing_capacity(self):
+        r""" Calculate the allowable bearing capacity of the mat foundation.
+
+        .. note::
+
+            Allowable bearing capacity:
+
+            .. math:: q_a(kPa) = 8 N f_d\left(\dfrac{S}{25.4}\right)
+
+            Depth factor:
+
+            .. math:: f_d = 1 + 0.33 \cdot \frac{D_f}{B} \le 1.33
+        """
         n_corr = self.corrected_spt_number
         return 8 * n_corr * self._fd() * self._sr()
 
 
 class TerzaghiABC4PadFoundation(AllowableBearingCapacity):
-    r"""Allowable bearing capacity for pad foundation on cohesionless
+    """Allowable bearing capacity for pad foundation on cohesionless
     soils according to ``Terzaghi & Peck (1948)``.
-
-    Notes
-    -----
-    Allowable bearing capacity for ``isolated/pad/spread`` foundations:
-
-    .. math::
-
-        q_a(kPa) &= 12N \dfrac{1}{c_w f_d}\left(\dfrac{S}{25.4}\right),
-                    \ B \ \le 1.2m
-
-        q_a(kPa) &= 8N\left(\dfrac{3.28B + 1}{3.28B} \right)^2\dfrac{1}
-                    {c_w f_d}\left(\dfrac{S}{25.4}\right), \ B \ \gt 1.2m
-
-    Depth factor:
-
-    .. math:: f_d = 1 + 0.25 \cdot \frac{D_f}{B} \le 1.25
-
-    Water correction for surface footing:
-
-    .. math:: c_w = 2 - \frac{D_w}{2B} \le 2
-
-    Water correction for fully submerged footing :math:`D_w \le D_f`
-
-    .. math:: c_w = 2 - \frac{D_f}{2B} \le 2
     """
 
     def __init__(self, corrected_spt_number: float, tol_settlement: float,
                  water_depth: float, foundation_size: FoundationSize) -> None:
         """
-        :param float corrected_spt_number: Lowest (or average) uncorrected SPT
-            N-value (60% energy) within the foundation influence zone i.e
-            :math:`D_f` to :math:`D_f + 2B`
-        :param float tol_settlement: Tolerable settlement of foundation.
-        :param float water_depth: Depth of water below ground surface.
-        :param FoundationSize foundation_size: Size of foundation.
+        :param corrected_spt_number: Lowest (or average) uncorrected SPT N-value 
+                                     (60% energy) within the foundation 
+                                     influence zone i.e :math:`D_f` to 
+                                     :math:`D_f + 2B`
+        :type corrected_spt_number: float
+
+        :param tol_settlement: Tolerable settlement of foundation.
+        :type tol_settlement: float
+
+        :param water_depth: Depth of water below ground surface (mm).
+        :type water_depth: float
+
+        :param foundation_size: Size of foundation.
+        :type foundation_size: FoundationSize
         """
         super().__init__(corrected_spt_number, tol_settlement, foundation_size)
         self.water_depth = water_depth
@@ -238,6 +243,32 @@ class TerzaghiABC4PadFoundation(AllowableBearingCapacity):
 
     @round_
     def bearing_capacity(self):
+        r""" Calculate the allowable bearing capacity of the pad foundation. 
+
+        .. note::
+
+            Allowable bearing capacity:
+
+            .. math::
+
+                q_a(kPa) &= 12N \dfrac{1}{c_w f_d}\left(\dfrac{S}{25.4}\right),
+                            \ B \ \le 1.2m
+
+                q_a(kPa) &= 8N\left(\dfrac{3.28B + 1}{3.28B} \right)^2\dfrac{1}
+                            {c_w f_d}\left(\dfrac{S}{25.4}\right), \ B \ \gt 1.2m
+
+            Depth factor:
+
+            .. math:: f_d = 1 + 0.25 \cdot \frac{D_f}{B} \le 1.25
+
+            Water correction for surface footing:
+
+            .. math:: c_w = 2 - \frac{D_w}{2B} \le 2
+
+            Water correction for fully submerged footing :math:`D_w \le D_f`
+
+            .. math:: c_w = 2 - \frac{D_f}{2B} \le 2
+        """
         n_corr = self.corrected_spt_number
         f_w = self.foundation_size.width
 
@@ -249,30 +280,32 @@ class TerzaghiABC4PadFoundation(AllowableBearingCapacity):
 
 
 class TerzaghiABC4MatFoundation(TerzaghiABC4PadFoundation):
-    r"""Allowable bearing capacity for mat foundation on cohesionless soils
+    """Allowable bearing capacity for mat foundation on cohesionless soils
     according to ``Terzaghi & Peck (1948)``.
-
-    Notes
-    -----
-    Allowable bearing capacity for ``isolated/pad/spread`` foundations:
-
-    .. math:: q_a(kPa) = 8N\dfrac{1}{c_w f_d}\left(\dfrac{S}{25.4}\right)
-
-    Depth factor:
-
-    .. math:: f_d = 1 + 0.25 \cdot \frac{D_f}{B} \le 1.25
-
-    Water correction for surface footing:
-
-    .. math:: c_w = 2 - \frac{D_w}{2B} \le 2
-
-    Water correction for fully submerged footing :math:`D_w \le D_f`
-
-    .. math:: c_w = 2 - \frac{D_f}{2B} \le 2
-
     """
 
     @round_
     def bearing_capacity(self):
+        r"""  Calculate the allowable bearing capacity of the mat foundation.
+
+        .. note::
+
+            Allowable bearing capacity:
+
+            .. math:: q_a(kPa) = 8N\dfrac{1}{c_w f_d}\left(\dfrac{S}{25.4}\right)
+
+            Depth factor:
+
+            .. math:: f_d = 1 + 0.25 \cdot \frac{D_f}{B} \le 1.25
+
+            Water correction for surface footing:
+
+            .. math:: c_w = 2 - \frac{D_w}{2B} \le 2
+
+            Water correction for fully submerged footing :math:`D_w \le D_f`
+
+            .. math:: c_w = 2 - \frac{D_f}{2B} \le 2
+        """
         n_corr = self.corrected_spt_number
         return 8 * n_corr * (1 / (self._cw() * self._fd())) * self._sr()
+
