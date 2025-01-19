0fe3b24e3bf45940a9e51b9c989af985f5eb0fde
Patrick Boateng <boatengpato.pb@gmail.com>
2025-01-18 23:45:21 -0600

    modified imports

--- tests/test_bearing_capacity/test_ubc_4_soils/test_vesic_ubc.py
+++ tests/test_bearing_capacity/test_ubc_4_soils/test_vesic_ubc.py
@@ -1,7 +1,6 @@
 import pytest
 
-from geolysis.bearing_capacity import Soil
-from geolysis.bearing_capacity.ubc_4_soils import VesicUltimateBearingCapacity
+from geolysis.bearing_capacity.ubc import VesicUltimateBearingCapacity
 
 from geolysis.foundation import create_foundation
 
@@ -11,8 +10,8 @@ ERROR_TOL = 0.01
 class TestVesicUBC:
     def test_bearing_capacity(self):
         fs = create_foundation(depth=1.0, width=1.5, eccentricity=0.2,
-                               footing_shape="square")
-        prop = Soil(friction_angle=0.0, cohesion=100.0, moist_unit_wgt=21.0)
+                               shape="square")
+        prop = dict(friction_angle=0.0, cohesion=100.0, moist_unit_wgt=21.0)
         ubc = VesicUltimateBearingCapacity(soil_properties=prop,
                                            foundation_size=fs)
         actual = ubc.bearing_capacity()
