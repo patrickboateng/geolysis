f3c67e9983e6fc9748968d29f739ad2e358281e8
Patrick Boateng <boatengpato.pb@gmail.com>
2025-03-03 15:50:51 -0600

    removed redundant function

--- geolysis/bearing_capacity/__init__.py
+++ geolysis/bearing_capacity/__init__.py
@@ -1,29 +1,5 @@
-from typing import TypeAlias
-
 from geolysis.bearing_capacity.abc.cohl import \
     create_allowable_bearing_capacity
-from geolysis.foundation import FoundationSize, Shape
-from geolysis.utils import isclose
 
 from .abc.cohl import bowles_abc, meyerhof_abc, terzaghi_abc
-
-
-FndParams: TypeAlias = tuple[float, float, Shape]
-
-
-def get_footing_params(foundation_size: FoundationSize) -> FndParams:
-    """Returns the ``width``, ``length``, and ``shape`` of the
-    foundation footing.
-
-    .. note:: "width" is the effective width of the foundation footing.
-    """
-    width = foundation_size.effective_width
-    length = foundation_size.length
-    shape = foundation_size.footing_shape
-
-    if not isclose(width, length) and shape != Shape.STRIP:
-        shape = Shape.RECTANGLE
-
-    return width, length, shape
-
 from .ubc import hansen_ubc, terzaghi_ubc, vesic_ubc
