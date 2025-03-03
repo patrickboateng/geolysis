e0c119dc58698c72668e75a7301fc7f085902cec
Patrick Boateng <boatengpato.pb@gmail.com>
2025-03-03 15:49:57 -0600

    added a common error msg template

--- geolysis/__init__.py
+++ geolysis/__init__.py
@@ -1,6 +1,11 @@
 __version__ = "0.4.2"
 
-# Error message template for common errors
-error_msg_tmpl = "{0} is not supported, Supported types are: {1}"
+
+def error_msg_tmpl(obj, objtype) -> str:
+    """Error message template for common errors."""
+    msg = "{0} is not supported, Supported types are: {1}"
+    objtypes = list(objtype)
+    return msg.format(obj, objtypes)
+
 
 from . import foundation, soil_classifier, spt, utils
