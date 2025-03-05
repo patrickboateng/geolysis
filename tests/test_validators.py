e8855d749f738b51d21a52bac4d65cbdfab11bce
Patrick Boateng <boatengpato.pb@gmail.com>
2025-03-05 15:20:12 -0600

    tests for validators

---
+++ tests/test_validators.py
@@ -0,0 +1,24 @@
+import pytest
+from geolysis.validators import le
+
+
+def test_le():
+    class C:
+
+        def __init__(self, foo):
+            self.foo = foo
+
+        @property
+        def foo(self):
+            return self._foo
+
+        @foo.setter
+        @le(25.4)
+        def foo(self, val):
+            self._foo = val
+
+    c = C(22.0)
+
+    with pytest.raises(ValueError):
+        foo = c.foo + 22.0
+        c.foo = foo
