28c515cf6a008c714103cbf216f8d3a6009f58df
Patrick Boateng <boatengpato.pb@gmail.com>
2025-03-29 11:32:48 -0500

    added more validators

--- geolysis/utils/validators.py
+++ geolysis/utils/validators.py
@@ -8,8 +8,7 @@ Number: TypeAlias = int | float
 def _num_validator(bound: float, /, *,
                    compare_symbol: str,
                    compare_fn: Callable,
-                   err_msg: str,
-                   exc_type: Callable):
+                   exc_type: Callable, err_msg: str):
     def dec(fn):
         def wrapper(obj, val):
             if not compare_fn(val, bound):
@@ -25,8 +24,7 @@ def _num_validator(bound: float, /, *,
 def _len_validator(bound: float, /, *,
                    compare_symbol: str,
                    compare_fn: Callable,
-                   err_msg: str,
-                   exc_type: Callable):
+                   exc_type: Callable, err_msg: str):
     def dec(fn):
         def wrapper(obj, val):
             _len = len(val)
@@ -43,38 +41,40 @@ def _len_validator(bound: float, /, *,
 def min_len(m_len: int, /, *, exc_type=ValueError, err_msg=None):
     return _len_validator(m_len, compare_symbol=">=",
                           compare_fn=operator.ge,
-                          err_msg=err_msg,
-                          exc_type=exc_type)
+                          exc_type=exc_type, err_msg=err_msg)
 
 
-# def lt(val: Number, /, *, exc_type=ValueError, err_msg=None):
-#     return _NumberValidator(val, "<", operator.lt, exc_type, err_msg)
+def lt(val: Number, /, *, exc_type=ValueError, err_msg=None):
+    return _num_validator(val, compare_symbol="<",
+                          compare_fn=operator.lt,
+                          exc_type=exc_type, err_msg=err_msg)
 
 
 def le(val: Number, /, *, exc_type=ValueError, err_msg=None):
     return _num_validator(val, compare_symbol="<=",
                           compare_fn=operator.le,
-                          err_msg=err_msg,
-                          exc_type=exc_type)
+                          exc_type=exc_type, err_msg=err_msg)
 
 
-# def eq(val: Number, /, *, exc_type=ValueError, err_msg=None):
-#     return _NumberValidator(val, "==", operator.eq, exc_type, err_msg)
+def eq(val: Number, /, *, exc_type=ValueError, err_msg=None):
+    return _num_validator(val, compare_symbol="==",
+                          compare_fn=operator.eq,
+                          exc_type=exc_type, err_msg=err_msg)
 
 
-# def ne(val: Number, /, *, exc_type=ValueError, err_msg=None):
-#     return _NumberValidator(val, "!=", operator.ne, exc_type, err_msg)
+def ne(val: Number, /, *, exc_type=ValueError, err_msg=None):
+    return _num_validator(val, compare_symbol="!=",
+                          compare_fn=operator.ne,
+                          exc_type=exc_type, err_msg=err_msg)
 
 
 def ge(val: Number, /, *, exc_type=ValueError, err_msg=None):
     return _num_validator(val, compare_symbol=">=",
                           compare_fn=operator.ge,
-                          err_msg=err_msg,
-                          exc_type=exc_type)
+                          exc_type=exc_type, err_msg=err_msg)
 
 
 def gt(val: Number, /, *, exc_type=ValueError, err_msg=None):
     return _num_validator(val, compare_symbol=">",
                           compare_fn=operator.gt,
-                          err_msg=err_msg,
-                          exc_type=exc_type)
+                          exc_type=exc_type, err_msg=err_msg)
--- tests/test_utils/test_validators.py
+++ tests/test_utils/test_validators.py
@@ -1,5 +1,5 @@
 import pytest
-from geolysis.utils.validators import le
+from geolysis.utils.validators import le, lt, eq, ne
 
 
 def test_le():
@@ -20,5 +20,71 @@ def test_le():
     c = C(22.0)
 
     with pytest.raises(ValueError):
-        foo = c.foo + 22.0
-        c.foo = foo
+        _val = c.foo + 22.0
+        c.foo = _val
+
+
+def test_lt():
+    class C:
+
+        def __init__(self, foo):
+            self.foo = foo
+
+        @property
+        def foo(self):
+            return self._foo
+
+        @foo.setter
+        @lt(25.4)
+        def foo(self, val):
+            self._foo = val
+
+    c = C(22.0)
+
+    with pytest.raises(ValueError):
+        _val = c.foo + 22.0
+        c.foo = _val
+
+
+def test_eq():
+    class C:
+
+        def __init__(self, foo):
+            self.foo = foo
+
+        @property
+        def foo(self):
+            return self._foo
+
+        @foo.setter
+        @eq(22)
+        def foo(self, val):
+            self._foo = val
+
+    c = C(22)
+
+    with pytest.raises(ValueError):
+        _val = c.foo
+        c.foo = _val + 22
+
+
+def test_ne():
+    class C:
+
+        def __init__(self, foo):
+            self.foo = foo
+
+        @property
+        def foo(self):
+            return self._foo
+
+        @foo.setter
+        @ne(22)
+        def foo(self, val):
+            self._foo = val
+
+    c = C(28)
+
+    with pytest.raises(ValueError):
+        _val = c.foo  # Just to make the getter for foo run
+        c.foo = 22
