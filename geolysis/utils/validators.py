bd6340134814e17291267815a75063d97475406d
Patrick Boateng <boatengpato.pb@gmail.com>
2025-04-10 21:49:53 -0500

    added custom validation error

--- geolysis/utils/validators.py
+++ geolysis/utils/validators.py
@@ -1,6 +1,8 @@
 """validators"""
 import operator
-from typing import Callable, TypeAlias
+from typing import Callable, TypeAlias, Optional
+
+from .exceptions import ValidationError
 
 Number: TypeAlias = int | float
 
@@ -8,7 +10,8 @@ Number: TypeAlias = int | float
 def _num_validator(bound: float, /, *,
                    compare_symbol: str,
                    compare_fn: Callable,
-                   exc_type: Callable, err_msg: str):
+                   exc_type: Callable,
+                   err_msg: str):
     def dec(fn):
         def wrapper(obj, val):
             if not compare_fn(val, bound):
@@ -24,7 +27,8 @@ def _num_validator(bound: float, /, *,
 def _len_validator(bound: float, /, *,
                    compare_symbol: str,
                    compare_fn: Callable,
-                   exc_type: Callable, err_msg: str):
+                   exc_type: Callable,
+                   err_msg: str):
     def dec(fn):
         def wrapper(obj, val):
             _len = len(val)
@@ -38,43 +42,57 @@ def _len_validator(bound: float, /, *,
     return dec
 
 
-def min_len(m_len: int, /, *, exc_type=ValueError, err_msg=None):
+def min_len(m_len: int, /, *,
+            exc_type=ValidationError,
+            err_msg: Optional[str] = None):
     return _len_validator(m_len, compare_symbol=">=",
                           compare_fn=operator.ge,
                           exc_type=exc_type, err_msg=err_msg)
 
 
-def lt(val: Number, /, *, exc_type=ValueError, err_msg=None):
+def lt(val: Number, /, *, exc_type=ValidationError,
+       err_msg: Optional[str] = None):
     return _num_validator(val, compare_symbol="<",
                           compare_fn=operator.lt,
-                          exc_type=exc_type, err_msg=err_msg)
+                          exc_type=exc_type,
+                          err_msg=err_msg)
 
 
-def le(val: Number, /, *, exc_type=ValueError, err_msg=None):
+def le(val: Number, /, *, exc_type=ValidationError,
+       err_msg: Optional[str] = None):
     return _num_validator(val, compare_symbol="<=",
                           compare_fn=operator.le,
-                          exc_type=exc_type, err_msg=err_msg)
+                          exc_type=exc_type,
+                          err_msg=err_msg)
 
 
-def eq(val: Number, /, *, exc_type=ValueError, err_msg=None):
+def eq(val: Number, /, *, exc_type=ValidationError,
+       err_msg: Optional[str] = None):
     return _num_validator(val, compare_symbol="==",
                           compare_fn=operator.eq,
-                          exc_type=exc_type, err_msg=err_msg)
+                          exc_type=exc_type,
+                          err_msg=err_msg)
 
 
-def ne(val: Number, /, *, exc_type=ValueError, err_msg=None):
+def ne(val: Number, /, *, exc_type=ValidationError,
+       err_msg: Optional[str] = None):
     return _num_validator(val, compare_symbol="!=",
                           compare_fn=operator.ne,
-                          exc_type=exc_type, err_msg=err_msg)
+                          exc_type=exc_type,
+                          err_msg=err_msg)
 
 
-def ge(val: Number, /, *, exc_type=ValueError, err_msg=None):
+def ge(val: Number, /, *, exc_type=ValidationError,
+       err_msg: Optional[str] = None):
     return _num_validator(val, compare_symbol=">=",
                           compare_fn=operator.ge,
-                          exc_type=exc_type, err_msg=err_msg)
+                          exc_type=exc_type,
+                          err_msg=err_msg)
 
 
-def gt(val: Number, /, *, exc_type=ValueError, err_msg=None):
+def gt(val: Number, /, *, exc_type=ValidationError,
+       err_msg: Optional[str] = None):
     return _num_validator(val, compare_symbol=">",
                           compare_fn=operator.gt,
-                          exc_type=exc_type, err_msg=err_msg)
+                          exc_type=exc_type,
+                          err_msg=err_msg)
