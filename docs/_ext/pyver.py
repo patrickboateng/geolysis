c6f9956241789bb459a6fcbca4390413c6baff8c
Patrick Boateng <boatengpato.pb@gmail.com>
2025-05-06 00:09:29 -0500

    refactored extension

--- docs/_ext/pyver.py
+++ docs/_ext/pyver.py
@@ -2,9 +2,6 @@ import re
 from docutils import nodes, utils
 
 
-# from docutils.parsers.rst.roles import set_classes
-
-
 def pyver_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
     """Link to a python version."""
     semver_rgx = re.compile(r'^'
@@ -15,33 +12,23 @@ def pyver_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
                             )
     mtch = semver_rgx.match(text)
 
-    if mtch:
-        pyver = mtch.group(0)
-    else:
-        msg = inliner.reporter.error(f"Invalid python version: {text:r}",
+    if not mtch:
+        msg = inliner.reporter.error(f"Invalid python version: {text!r}",
                                      line=lineno)
         prb = inliner.problematic(rawtext, rawtext, msg)
         return [prb], [msg]
-    app = inliner.document.settings.env.app
-    node = make_link_node(rawtext, app, pyver, options)
+
+    pyver = mtch.group(0)
+    pyver_docs_page = "https://docs.python.org/3/whatsnew"
+    refuri = pyver_docs_page + '/' + f"{pyver}.html"
+    node = nodes.reference(rawtext,
+                           utils.unescape(pyver),
+                           refuri=refuri,
+                           classes=["external-link"],
+                           **options)
     return [node], []
-
-
-def make_link_node(rawtext, app, slug, options):
-    try:
-        py_docs_url = app.config.python_docs_url
-        if not py_docs_url:
-            raise AttributeError
-    except AttributeError:
-        raise ValueError("python_docs_url not set")
-
-    refuri = py_docs_url + '/' + f"{slug}.html"
-    node = nodes.reference(rawtext, utils.unescape(slug),
-                           refuri=refuri, classes=["external-link"], **options)
-    return node
 
 
 def setup(app):
     app.add_role('pyver', pyver_role)
-    app.add_config_value('python_docs_url', None, 'env')
-    return
+    # app.add_config_value('python_docs_url', None, 'env')
