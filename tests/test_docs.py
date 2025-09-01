32f46409b4dde4f577742241a1c0d12b6e14fa75
Patrick Boateng <boatengpato.pb@gmail.com>
2025-08-31 19:10:00 -0500

    minor refactoring

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -2,12 +2,9 @@ import pathlib
 
 from mktestdocs import check_md_file
 
-fpath = pathlib.Path("docs")
+file_1 = pathlib.Path("docs") / "dev_guide" / "style_guide.md"
 
-check_md_file(fpath=fpath / "dev_guide" / "style_guide.md")
+_test_files = [file_1]
 
-# @pytest.mark.parametrize('fpath',
-#                          pathlib.Path("docs"),
-#                          ids=str)
-# def test_check_md_file(fpath):
-#     check_md_file(fpath=fpath / "style_guide.md", memory=True)
+for file_path in _test_files:
+    check_md_file(fpath=file_path)
