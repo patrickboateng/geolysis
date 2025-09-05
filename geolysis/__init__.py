9db60384f0a9422daa0501ac81fdc14fa401e823
Patrick Boateng <boatengpato.pb@gmail.com>
2025-09-05 16:24:53 -0500

    v0.10.2

--- geolysis/__init__.py
+++ geolysis/__init__.py
@@ -1,5 +1,5 @@
 from . import bearing_capacity, foundation, soil_classifier, spt
 
-__version__ = "0.10.1"
+__version__ = "0.10.2"
 
 __all__ = ["foundation", "soil_classifier", "spt", "bearing_capacity"]
