30a8f95eac87248817e7f72e47869554983d8dc1
Patrick Boateng <boatengpato.pb@gmail.com>
2025-09-13 15:39:27 -0500

    bumped version to 0.10.4

--- geolysis/__init__.py
+++ geolysis/__init__.py
@@ -1,5 +1,5 @@
 from . import bearing_capacity, foundation, soil_classifier, spt
 
-__version__ = "0.10.3"
+__version__ = "0.10.4"
 
 __all__ = ["foundation", "soil_classifier", "spt", "bearing_capacity"]
