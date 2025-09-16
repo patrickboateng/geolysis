89ab3747cf2855a1c90ae5e2796dc589abdac477
Patrick Boateng <boatengpato.pb@gmail.com>
2025-09-16 14:13:09 -0500

    fixed error in tests

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -2,7 +2,7 @@ import pathlib
 
 from mktestdocs import check_md_file
 
-file_1 = pathlib.Path("docs") / "dev_guide" / "style_guide.md"
+file_1 = pathlib.Path("docs") / "index.md"
 
 _test_files = [file_1]
 
