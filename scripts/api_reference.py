c3d64592a34f04c31fd8fe74c4b5c19b76931970
Patrick Boateng <boatengpato.pb@gmail.com>
2025-11-04 20:48:13 -0600

    names of modules to document

---
+++ scripts/api_reference.py
@@ -0,0 +1,23 @@
+API_REFERENCE = {
+    "geolysis.bearing_capacity.abc": {
+        "short_summary": "Allowable bearing capacity classes.",
+    },
+    "geolysis.bearing_capacity.ubc": {
+        "short_summary": "Ultimate bearing capacity classes.",
+    },
+    "geolysis.foundation": {
+        "short_summary": "Foundation classes.",
+    },
+    "geolysis.soil_classifier": {
+        "short_summary": "Soil classifier classes.",
+    },
+    "geolysis.spt": {
+        "short_summary": "SPT classes.",
+    },
+    # "geolysis.exceptions": {
+    #     "short_summary": "Exception classes.",
+    # },
+    "geolysis.utils": {
+        "short_summary": "Utilities classes.",
+    },
+}
